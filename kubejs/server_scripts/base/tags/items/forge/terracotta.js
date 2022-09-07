4c636203bc87ef7e8d36df5cee057cb40021b2ce
MuteTiefling <mutetiefling@gmail.com>
2022-09-07 07:09:19 -0400

    fix terracotta tags

--- kubejs/server_scripts/base/tags/items/forge/terracotta.js
+++ kubejs/server_scripts/base/tags/items/forge/terracotta.js
@@ -2,4 +2,5 @@ ServerEvents.tags('item', (event) => {
     colors.forEach((color) => {
         event.get(`forge:terracotta/${color}`).add(`minecraft:${color}_terracotta`);
     });
+    event.get(`forge:terracotta/plain`).add(`minecraft:terracotta`);
 });
