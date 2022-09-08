ad7832efcf5b9a25f52a7e5a3b7189f284669faa
MuteTiefling <mutetiefling@gmail.com>
2022-09-08 17:26:17 -0400

    Create shaped.js

---
+++ kubejs/server_scripts/base/recipes/explorerscompass/shaped.js
@@ -0,0 +1,20 @@
+ServerEvents.recipes((event) => {
+    const id_prefex = 'enigmatica:base/explorerscompass/shaped/';
+
+    const recipes = [
+        {
+            output: 'explorerscompass:explorerscompass',
+            pattern: ['ABA', 'BCB', 'ABA'],
+            key: {
+                A: '#blue_skies:vines',
+                B: '#blue_skies:gems/moonstone',
+                C: '#forge:compasses'
+            },
+            id: `explorerscompass:explorers_compass`
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
+    });
+});
