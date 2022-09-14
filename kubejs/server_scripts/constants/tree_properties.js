79c5c8ea2d9e54ee0b914bd33f90769a9006cd25
MuteTiefling <mutetiefling@gmail.com>
2022-09-13 21:06:17 -0400

    init

---
+++ kubejs/server_scripts/constants/tree_properties.js
@@ -0,0 +1,46 @@
+//priority: 1000
+
+const treeProperties = [
+    {
+        trees: [
+            {
+                sapling: 'minecraft:oak_sapling',
+                homeDimension: 'overworld',
+                logs: ['minecraft:oak_log'],
+                leaves: ['minecraft:oak_leaves'],
+                fruit: ['minecraft:apple'],
+                substrate: 'dirt',
+                sap: 'thermal:resin',
+                rate: { living: 25, dead: 4 }
+            }
+        ],
+        shrooms: [
+            {
+                sapling: 'minecraft:brown_mushroom',
+                trunk: 'minecraft:mushroom_stem',
+                leaf: 'minecraft:brown_mushroom_block',
+                substrate: 'mushroom'
+            },
+            {
+                sapling: 'minecraft:crimson_fungus',
+                trunk: 'minecraft:crimson_stem',
+                leaf: 'minecraft:nether_wart_block',
+                extraDecoration: 'minecraft:shroomlight',
+                substrate: 'crimson_nylium'
+            },
+            {
+                sapling: 'minecraft:red_mushroom',
+                trunk: 'minecraft:mushroom_stem',
+                leaf: 'minecraft:red_mushroom_block',
+                substrate: 'mushroom'
+            },
+            {
+                sapling: 'minecraft:warped_fungus',
+                trunk: 'minecraft:warped_stem',
+                leaf: 'minecraft:warped_wart_block',
+                extraDecoration: 'minecraft:shroomlight',
+                substrate: 'warped_nylium'
+            }
+        ]
+    }
+];
