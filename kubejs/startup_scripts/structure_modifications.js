0e9d79712e16962919a7efcfcf2cf36911540794
MuteTiefling <mutetiefling@gmail.com>
2022-09-23 18:19:46 -0400

    replace RS engineering in all IE structures for Expert

--- kubejs/startup_scripts/structure_modifications.js
+++ kubejs/startup_scripts/structure_modifications.js
@@ -4,7 +4,47 @@ const structures = [
         mod: 'immersiveengineering',
         type: 'multiblocks',
         name: 'arcfurnace',
-        index: 4,
+        index: 7,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'kubejs:disabled_structure_indicator'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'assembler',
+        index: 1,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'minecraft:stone'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'auto_workbench',
+        index: 2,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'minecraft:stone'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'bottling_machine',
+        index: 2,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'minecraft:stone'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'crusher',
+        index: 3,
         block: {
             normal: 'immersiveengineering:rs_engineering',
             expert: 'minecraft:stone'
@@ -15,6 +55,76 @@ const structures = [
         type: 'multiblocks',
         name: 'diesel_generator',
         index: 4,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'minecraft:stone'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'excavator',
+        index: 5,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'minecraft:stone'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'excavator_full',
+        index: 6,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'minecraft:stone'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'fermenter',
+        index: 4,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'minecraft:stone'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'metal_press',
+        index: 2,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'minecraft:stone'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'mixer',
+        index: 4,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'minecraft:stone'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'refinery',
+        index: 5,
+        block: {
+            normal: 'immersiveengineering:rs_engineering',
+            expert: 'minecraft:stone'
+        }
+    },
+    {
+        mod: 'immersiveengineering',
+        type: 'multiblocks',
+        name: 'sawmill',
+        index: 5,
         block: {
             normal: 'immersiveengineering:rs_engineering',
             expert: 'minecraft:stone'
