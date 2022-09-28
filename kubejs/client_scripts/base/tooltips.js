7f055a387519847193bde332b3c2eca07c27c3d8
MuteTiefling <mutetiefling@gmail.com>
2022-09-27 21:32:56 -0400

    More Helper Tooltips

--- kubejs/client_scripts/base/tooltips.js
+++ kubejs/client_scripts/base/tooltips.js
@@ -30,6 +30,34 @@ ItemEvents.tooltip((event) => {
             text: [Text.of('Recipe disabled for Enigmatica 9').gold()]
         },
         {
+            items: ['quark:ender_watcher'],
+            text: [Text.of('Redstone Device: Detects Players Looking').darkRed()]
+        },
+        {
+            items: ['quark:obsidian_pressure_plate'],
+            text: [Text.of('Redstone Device: Detects Players').darkRed()]
+        },
+        {
+            items: ['quark:abacus'],
+            text: [Text.of('Measuring Tool').gold()]
+        },
+        {
+            items: ['quark:grate'],
+            text: [Text.of('Allows Items to Pass').gold()]
+        },
+        {
+            items: ['quark:trowel'],
+            text: [Text.of('Building Tool: Random Block Placement').gold()]
+        },
+        {
+            items: ['quark:iron_button'],
+            text: [Text.of('Redstone Device: Long Pulse').darkRed()]
+        },
+        {
+            items: ['quark:gold_button'],
+            text: [Text.of('Redstone Device: Short Pulse').darkRed()]
+        },
+        {
             items: [
                 'naturesaura:animal_generator',
                 'naturesaura:projectile_generator',
@@ -44,7 +72,7 @@ ItemEvents.tooltip((event) => {
             text: [Text.of(`Arcane Apparatus: Aura Generator`).green()]
         },
         {
-            items: ['naturesaura:light_staff'],
+            items: ['naturesaura:light_staff', 'ars_nouveau:jar_of_light'],
             text: [Text.of(`Arcane Instrument: Creates Light Sources`).gold()]
         },
         {
@@ -144,8 +172,77 @@ ItemEvents.tooltip((event) => {
             text: [Text.of(`Arcane Apparatus: Ore Processing`).green()]
         },
         {
-            items: ['naturesaura:ender_crate', 'naturesaura:ender_access', 'minecraft:ender_chest'],
+            items: ['naturesaura:range_visualizer'],
+            text: [Text.of(`Arcane Instrument: Visualizes Apparatus Effect Area`).green()]
+        },
+        {
+            items: [
+                'naturesaura:ender_crate',
+                'naturesaura:ender_access',
+                'minecraft:ender_chest',
+                'functionalstorage:ender_drawer'
+            ],
             text: [Text.of(`Arcane Apparatus: Cross-Dimensional Item Transportation`).darkPurple()]
+        },
+        {
+            items: ['ars_nouveau:enchanters_mirror'],
+            text: [Text.of(`Arcane Focus: Self`).gold()]
+        },
+        {
+            items: ['ars_elemental:spell_horn'],
+            text: [Text.of(`Arcane Focus: Area`).gold()]
+        },
+        {
+            items: ['ars_nouveau:wand'],
+            text: [Text.of(`Arcane Focus: Projectile`).gold()]
+        },
+        {
+            items: ['ars_nouveau:enchanters_sword'],
+            text: [Text.of(`Arcane Focus: Touch`).gold()]
+        },
+        {
+            items: ['ars_nouveau:enchanters_shield'],
+            text: [Text.of(`Arcane Instrument: Blocked Attacks Boost Spell Damage and Mana Regen`).lightPurple()]
+        },
+        {
+            items: ['ars_nouveau:splash_flask_cannon'],
+            text: [Text.of(`Arcane Instrument: Converts Potions to Splash Potions`).lightPurple()]
+        },
+        {
+            items: ['ars_nouveau:lingering_flask_cannon'],
+            text: [Text.of(`Arcane Instrument: Converts Potions to Lingering Potions`).lightPurple()]
+        },
+        {
+            items: ['ars_nouveau:void_jar'],
+            text: [Text.of(`Arcane Instrument: Converts Items to Source`).lightPurple()]
+        },
+        {
+            items: ['ars_nouveau:potion_diffuser'],
+            text: [Text.of(`Arcane Apparatus: Applies Potions in the Area`).lightPurple()]
+        },
+        {
+            items: ['ars_nouveau:belt_of_levitation'],
+            text: [Text.of(`Arcane Instrument: Allows the Wearer to Hover`).lightPurple()]
+        },
+        {
+            items: ['ars_nouveau:belt_of_unstable_gifts'],
+            text: [Text.of(`Arcane Instrument: Grants Random Potion Effects`).lightPurple()]
+        },
+        {
+            items: ['ars_nouveau:ring_of_lesser_discount', 'ars_nouveau:ring_of_greater_discount'],
+            text: [Text.of(`Arcane Instrument: Reduces Spell Cost`).lightPurple()]
+        },
+        {
+            items: ['ars_nouveau:amulet_of_mana_boost'],
+            text: [Text.of(`Arcane Instrument: Increases Personal Mana Pool`).lightPurple()]
+        },
+        {
+            items: ['ars_nouveau:amulet_of_mana_regen'],
+            text: [Text.of(`Arcane Instrument: Increases Personal Mana Regen Rate`).lightPurple()]
+        },
+        {
+            items: ['ars_nouveau:dominion_wand'],
+            text: [Text.of(`Arcane Instrument: Configuration Tool`).lightPurple()]
         }
 
         /*
