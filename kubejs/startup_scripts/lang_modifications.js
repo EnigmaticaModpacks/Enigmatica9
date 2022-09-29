d1a5d2cc7befc36610d570b28156297cf3193ad8
MuteTiefling <mutetiefling@gmail.com>
2022-09-28 22:09:05 -0400

    Rename items by pack mode
    Similar to structure modification script, this'll apply a name change to items by pack mode. This gives us the freedom to rename things for Expert without worrying about what it looks like for Normal.

---
+++ kubejs/startup_scripts/lang_modifications.js
@@ -0,0 +1,31 @@
+//priority: 800
+const entries = [
+    {
+        key: 'block.naturesaura.infused_iron_block',
+        value: {
+            normal: 'Infused Iron Block',
+            expert: 'Infused Ironwood Block'
+        }
+    },
+    {
+        key: 'item.naturesaura.infused_iron',
+        value: {
+            normal: 'Infused Iron Ingot',
+            expert: 'Infused Ironwood Ingot'
+        }
+    },
+    {
+        key: 'advancement.naturesaura.infused_materials.desc',
+        value: {
+            normal: 'Use the Natural Altar to create Infused Iron',
+            expert: 'Use the Natural Altar to create Infused Ironwood'
+        }
+    }
+];
+
+entries.forEach((entry) => {
+    let lang_file = `kubejs/assets/enigmatica/lang/en_us.json`;
+    let lang_json = JsonIO.read(lang_file);
+    lang_json[entry.key] = entry.value[packMode];
+    JsonIO.write(lang_file, lang_json);
+});
