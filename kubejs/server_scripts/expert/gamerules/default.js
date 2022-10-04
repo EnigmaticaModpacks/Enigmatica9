2f63dda2d120a81ca07cf173ad8d91432d39c8cd
MuteTiefling <mutetiefling@gmail.com>
2022-10-04 14:34:45 -0400

    Update default.js

--- kubejs/server_scripts/expert/gamerules/default.js
+++ kubejs/server_scripts/expert/gamerules/default.js
@@ -2,7 +2,8 @@ ServerEvents.loaded((event) => {
     if (global.isExpertMode == false) {
         return;
     }
-    if (!event.server.persistentData.hasLoaded) {
+    if (!event.server.persistentData.defaultGamerulesLoaded) {
+        event.server.persistentData.defaultGamerulesLoaded = 1;
         let gamerules = ['tfEnforcedProgression false'];
 
         gamerules.forEach((gamerule) => {
