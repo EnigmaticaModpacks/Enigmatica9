75f2af829dd8263f94eca542a936c32161695e36
MuteTiefling <mutetiefling@gmail.com>
2022-10-04 05:56:50 -0400

    helper tooltips, add missing lang entries for rituals

--- kubejs/server_scripts/constants/ritual_effects.js
+++ kubejs/server_scripts/constants/ritual_effects.js
@@ -49,10 +49,45 @@ const ritual_effects = {
         potion: {
             spells: [
                 {
+                    // Maximum number of entities to affect
                     limit: 3,
+                    // Diameter of selection area, centered on ritual
                     range: 10,
+                    // Effect to apply
                     effect: 'minecraft:slow_falling',
+                    // Effect Duration in seconds
                     duration: 60,
+                    // Effect level. 1 means 1
+                    level: 1
+                }
+            ]
+        }
+    },
+    teleport_everbright: {
+        teleport: {
+            // Where are we going?
+            arrival: 'blue_skies:everbright',
+            // Only allow departure from these dimensions
+            departure: ['blue_skies:everdawn', 'twilightforest:twilight_forest'],
+            // How random the teleport destination is
+            uncertainty: 5000,
+            // Maximum number of entities to affect
+            limit: 3,
+            // Diameter of selection area, centered on ritual
+            range: 10
+        },
+        potion: {
+            spells: [
+                {
+                    // Maximum number of entities to affect
+                    limit: 3,
+                    // Diameter of selection area, centered on ritual
+                    range: 10,
+                    // Effect to apply
+                    effect: 'minecraft:slow_falling',
+                    // Effect Duration in seconds
+                    duration: 60,
+                    // Effect level. 1 means 1
                     level: 1
                 }
             ]
