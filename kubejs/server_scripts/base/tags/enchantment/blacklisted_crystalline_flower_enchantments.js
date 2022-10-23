50e46f4aec8f7f4ae8fdf3532e9f4c667f4f5b6f
MuteTiefling <mutetiefling@gmail.com>
2022-10-22 21:01:40 -0400

    apotheosis

---
+++ kubejs/server_scripts/base/tags/enchantment/blacklisted_crystalline_flower_enchantments.js
@@ -0,0 +1,18 @@
+ServerEvents.tags('enchantment', (event) => {
+    let enchantments = [
+        'apotheosis:earths_boon',
+        'apotheosis:infusion',
+        'apotheosis:rebounding',
+        'apotheosis:spearfishing',
+        'ars_elemental:mirror_shield',
+        'ars_nouveau:reactive',
+        'minecraft:binding_curse',
+        'minecraft:respiration',
+        'minecraft:soul_speed',
+        'minecraft:swift_sneak',
+        'minecraft:vanishing_curse',
+        'supplementaries:stasis'
+    ];
+
+    event.get('the_bumblezone:blacklisted_crystalline_flower_enchantments').add(enchantments);
+});
