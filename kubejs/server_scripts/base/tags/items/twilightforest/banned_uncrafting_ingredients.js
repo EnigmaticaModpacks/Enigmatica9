ebb21d5dfdb6cfee3b66bf1119f05eda63471c84
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2022-11-03 21:47:24 +0100

    removed `banned_uncrafting_ingredients` tag from everything
    It serves no purpose since the category + uncrafting table is disabled, and is just clutter-y

--- kubejs/server_scripts/base/tags/items/twilightforest/banned_uncrafting_ingredients.js
+++ kubejs/server_scripts/base/tags/items/twilightforest/banned_uncrafting_ingredients.js
@@ -1,3 +1,3 @@
 ServerEvents.tags('item', (event) => {
-    event.get('twilightforest:banned_uncrafting_ingredients').add(/.*/);
+    event.get('twilightforest:banned_uncrafting_ingredients').remove(/.*/);
 });
