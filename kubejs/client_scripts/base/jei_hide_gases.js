fb4de178460dfc44415fb2cc668ff39a404f2598
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2022-11-26 17:44:26 +0100

    Fix hiding Gasses/slurries in JEI

--- kubejs/client_scripts/base/jei_hide_gases.js
+++ kubejs/client_scripts/base/jei_hide_gases.js
@@ -1,4 +1,4 @@
-const MekanismJEI = Java.loadClass('mekanism.client.jei.MekanismJEI')
+let MekanismJEI = Java.loadClass('mekanism.client.jei.MekanismJEI')
 JEIEvents.hideCustom(event => {
     jei.base.gases.hidden.forEach((hiddenGas) => {
         event.get(MekanismJEI.TYPE_GAS).hide(hiddenGas)
