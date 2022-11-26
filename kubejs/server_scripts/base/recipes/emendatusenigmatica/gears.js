272ca1502b1f5b914aa38574a359e0770fc8baf5
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2022-11-26 23:57:22 +0100

    More EE Unification setup

--- kubejs/server_scripts/base/recipes/emendatusenigmatica/gears.js
+++ kubejs/server_scripts/base/recipes/emendatusenigmatica/gears.js
@@ -23,23 +23,13 @@ ServerEvents.recipes((event) => {
 
     let props = Object.keys(metal_properties);
     props.forEach((metal) => {
-        if (Item.exists(`emendatusenigmatica:${metal}_rod`)) {
-            recipes.push(
-                {    
-                    output: Item.of(`emendatusenigmatica:${metal}_rod`,4),
-                    pattern: [' A ', ' A '],
-                    key: {
-                        A: `#forge:ingots/${metal}`,
-                    },
-                    id: `emendatusenigmatica:rod/from_ingot/${metal}`
-                }
-            )
+        if (Item.exists(`emendatusenigmatica:${metal}_gear`)) {
             // Remove other recipes
             // Reason for this is pretty simple, AU doesn't unify recipes that are created / modified by KubeJS, so we need to remove unused recipes manually >.>
             // OR recipe is a bit different, but if we modify it, it will not be Unified as well ^^"
-            if (Item.exists(`immersiveengineering:stick_${metal}`)) {
+            if (Item.exists(`industrialforegoing:${metal}_gear`)) {
                 recipes_to_remove.push({
-                    id: `immersiveengineering:crafting/stick_${metal}`
+                    id: `industrialforegoing:${metal}_gear`
                 })
             }
         }
