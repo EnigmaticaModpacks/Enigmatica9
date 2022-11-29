c372e5d4528e352409e21718291e0953ebb0fe9e
MuteTiefling <mutetiefling@gmail.com>
2022-11-28 19:07:40 -0500

    start quest work, elementals tag

--- kubejs/server_scripts/base/tags/entity_type/enigmatica/elementals.js
+++ kubejs/server_scripts/base/tags/entity_type/enigmatica/elementals.js
@@ -1,8 +1,8 @@
 ServerEvents.tags('entity_type', (event) => {
-    // const entities = ['minecraft:blaze', 'thermal:blizz', 'thermal:blitz', 'thermal:basalz'];
-    // event.get('enigmatica:elementals').add(entities);
-    // event.get('enigmatica:elementals/fire').add('minecraft:blaze');
-    // event.get('enigmatica:elementals/water').add('thermal:blizz');
-    // event.get('enigmatica:elementals/air').add('thermal:blitz');
-    // event.get('enigmatica:elementals/earth').add('thermal:basalz');
+    const entities = ['minecraft:blaze', 'thermal:blizz', 'thermal:blitz', 'thermal:basalz'];
+    event.get('enigmatica:elementals').add(entities);
+    event.get('enigmatica:elementals/fire').add('minecraft:blaze');
+    event.get('enigmatica:elementals/water').add('thermal:blizz');
+    event.get('enigmatica:elementals/air').add('thermal:blitz');
+    event.get('enigmatica:elementals/earth').add('thermal:basalz');
 });
