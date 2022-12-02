8f008cca5e658b77d2aca0a95e724dd61456ad15
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2022-12-03 00:58:27 +0100

    Remove soul steel from unification
    It has different textures and all, what I'm pretty sure isn't possible to do with EE :sweat:

--- kubejs/server_scripts/base/tags/items/spirit/soul_steel.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.tags('item', (event) => {
-    event.add("forge:ingots/soul_steel", "spirit:soul_steel_ingot")
-    event.add("forge:storage_blocks/soul_steel", "spirit:soul_steel_block")
-})
