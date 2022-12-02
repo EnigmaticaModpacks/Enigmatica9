e45f07edc4c553e201b6f009759dcbf86a4ed043
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2022-12-02 23:46:35 +0100

    Unify new Materials

---
+++ kubejs/server_scripts/base/tags/items/spirit/soul_steel.js
@@ -0,0 +1,4 @@
+ServerEvents.tags('item', (event) => {
+    event.add("forge:ingots/soul_steel", "spirit:soul_steel_ingot")
+    event.add("forge:storage_blocks/soul_steel", "spirit:soul_steel_block")
+})
