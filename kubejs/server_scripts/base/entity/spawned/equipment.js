b0755686011713159641ab38fca96cd29dfb92d1
MuteTiefling <mutetiefling@gmail.com>
2022-12-03 22:43:45 -0500

    revert check for creeper overhaul creepers as it crashes the script on load...

--- kubejs/server_scripts/base/entity/spawned/equipment.js
+++ kubejs/server_scripts/base/entity/spawned/equipment.js
@@ -14,12 +14,10 @@ EntityEvents.spawned((event) => {
     // Don't apply buffs to Spirit spawner mobs
     // Don't apply buffs to Apotheosis bosses
     // Don't re-apply buffs to already checked mobs
-    // Creeper Overhaul entities don't give the correct `type` output. This appears to be the only way to ignore them...
     if (
         event.entity.fullNBT.hasOwnProperty('Corrupted') ||
         event.entity.fullNBT.ForgeData.hasOwnProperty('apoth.boss') ||
-        event.entity.fullNBT.ForgeData.hasOwnProperty('enigmatica_equipment') ||
-        event.entity.type.texture
+        event.entity.fullNBT.ForgeData.hasOwnProperty('enigmatica_equipment')
     ) {
         return;
     }
