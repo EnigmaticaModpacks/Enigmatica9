03e8b34b202f537dbfa065d9ce198eb0d7b57ceb
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2022-12-06 13:08:37 +0100

    Yeet variable from crushed_ores.js

--- kubejs/server_scripts/base/tags/items/create/crushed_ores.js
+++ kubejs/server_scripts/base/tags/items/create/crushed_ores.js
@@ -1,7 +1,5 @@
 ServerEvents.tags('item', (event) => {
-    let props = Object.keys(metal_properties);
-
-    props.forEach((metal) => {
+    Object.keys(metal_properties).forEach((metal) => {
         if (Item.exists(`create:crushed_${metal}_ore`)) {
             event.get(`create:crushed_ores/${metal}`).add(`create:crushed_${metal}_ore`);
         }
