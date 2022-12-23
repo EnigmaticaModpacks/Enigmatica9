c00607aadca141e41089c692c1c7b5c1d7175ee8
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2022-12-23 14:44:24 +0100

    WIP Stuff / Dust Unification

--- kubejs/server_scripts/base/recipes/emendatusenigmatica/dusts.js
+++ kubejs/server_scripts/base/recipes/emendatusenigmatica/dusts.js
@@ -1,9 +1,10 @@
 ServerEvents.recipes((event) => {
     const recipes = [];
 
-    let props = Object.keys(metal_properties);
+    let metals = Object.keys(metal_properties);
+    let gems = Object.keys(gemProperties);
 
-    props.forEach((metal) => {
+    metals.forEach((metal) => {
         if (Item.exists(`emendatusenigmatica:raw_${metal}` && Item.exists(`emendatusenigmatica:${metal}_dust`))) {
             recipes.push(
                 {
@@ -20,7 +21,23 @@ ServerEvents.recipes((event) => {
         }
     });
 
+    gems.forEach((gem) => {
+        if (Item.exists(`emendatusenigmatica:${gem}_dust`)) {
+            recipes.push({
+                type: 'immersiveengineering:crusher',
+                energy: 20000,
+                input: Item.of(`emendatusenigmatica:${gem}_gem`).toJson(),
+                result: { base_ingredient: { item: `emendatusenigmatica:${gem}_dust` }, count: 1 },
+                id: `emendatusenigmatica:ie/crusher/gem/${gem}_dust`
+            })
+        }
+    })
+
     recipes.forEach((recipe) => {
-        event.shapeless(recipe.output, recipe.inputs).id(recipe.id);
+        if (recipe.type) {
+            event.custom(recipe).id(recipe.id)
+        } else {
+            event.shapeless(recipe.output, recipe.inputs).id(recipe.id);
+        }
     });
 });
