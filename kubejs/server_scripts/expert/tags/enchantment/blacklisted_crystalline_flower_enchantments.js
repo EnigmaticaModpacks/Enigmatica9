2ad5d2d9ce0ddc3fada02dd024a239b5c81ea511
MuteTiefling <mutetiefling@gmail.com>
2023-01-04 22:59:45 -0500

    update bumblezone tags for V.6.6.0 changes

--- kubejs/server_scripts/expert/tags/enchantment/blacklisted_crystalline_flower_enchantments.js
+++ kubejs/server_scripts/expert/tags/enchantment/blacklisted_crystalline_flower_enchantments.js
@@ -1,9 +1,1 @@
-ServerEvents.tags('enchantment', (event) => {
-    if (global.isExpertMode == false) {
-        return;
-    }
-
-    const enchantments = ['minecraft:respiration'];
-
-    event.get('the_bumblezone:blacklisted_crystalline_flower_enchantments').add(enchantments);
-});
+// TODO: Remove next breaking update
