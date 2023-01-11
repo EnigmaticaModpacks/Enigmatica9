755f3ed665c818ae3d7b462709d062173d827ce6
MuteTiefling <mutetiefling@gmail.com>
2023-01-11 13:42:03 -0500

    [Expert] Sample recipe for converting blocks while in water.
    requires water on four sides

---
+++ kubejs/server_scripts/expert/recipes/lychee/random_block_ticking.js
@@ -0,0 +1,125 @@
+ServerEvents.recipes((event) => {
+    if (global.isExpertMode == false) {
+        return;
+    }
+    // Predicate builder: https://misode.github.io/predicate/
+
+    const id_prefix = 'enigmatica:expert/lychee/random_block_ticking/';
+    const recipes = [
+        // Sample recipe for uranium filtering. Adjust chance to speed up or slow down.
+        {
+            block_in: { blocks: ['thermal:white_rockwool'] },
+            post: [
+                {
+                    type: 'place',
+                    block: 'thermal:lime_rockwool',
+                    contextual: [
+                        {
+                            type: 'and',
+                            contextual: [
+                                { type: 'location', predicate: { 'lychee:biome_tag': 'is_ocean' } },
+                                { type: 'chance', chance: 0.1 },
+                                {
+                                    type: 'location',
+                                    offsetX: -1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                },
+                                {
+                                    type: 'location',
+                                    offsetX: 1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                },
+                                {
+                                    type: 'location',
+                                    offsetZ: -1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                },
+                                {
+                                    type: 'location',
+                                    offsetZ: 1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    type: 'execute',
+                    command: 'playsound minecraft:block.water.ambient ambient @p ~ ~ ~',
+                    hide: true,
+                    contextual: [
+                        {
+                            type: 'and',
+                            contextual: [
+                                { type: 'location', predicate: { 'lychee:biome_tag': 'is_ocean' } },
+                                { type: 'chance', chance: 0.1 },
+                                {
+                                    type: 'location',
+                                    offsetX: -1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                },
+                                {
+                                    type: 'location',
+                                    offsetX: 1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                },
+                                {
+                                    type: 'location',
+                                    offsetZ: -1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                },
+                                {
+                                    type: 'location',
+                                    offsetZ: 1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    type: 'execute',
+                    command: 'playsound minecraft:block.wool.place block @p ~ ~ ~ 0.3 0.1',
+                    hide: true,
+                    contextual: [
+                        {
+                            type: 'and',
+                            contextual: [
+                                { type: 'location', predicate: { 'lychee:biome_tag': 'is_ocean' } },
+                                { type: 'chance', chance: 0.1 },
+                                {
+                                    type: 'location',
+                                    offsetX: -1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                },
+                                {
+                                    type: 'location',
+                                    offsetX: 1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                },
+                                {
+                                    type: 'location',
+                                    offsetZ: -1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                },
+                                {
+                                    type: 'location',
+                                    offsetZ: 1,
+                                    predicate: { block: { blocks: ['minecraft:water'] } }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+            id: `${id_prefix}filter_filling`
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        recipe.type = 'lychee:random_block_ticking';
+        recipe.item_in = Item.of(recipe.input).toJson();
+
+        event.custom(recipe).id(recipe.id);
+    });
+});
