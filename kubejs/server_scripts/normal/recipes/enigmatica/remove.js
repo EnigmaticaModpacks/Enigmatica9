c3ce16e19c1988f6f7894c49573e619f08594554
Kanzaji ^^ <60540476+Kanzaji@users.noreply.github.com>
2023-01-14 15:10:30 +0100

    Remove commented out lines from Normal/Expert remove.js

--- kubejs/server_scripts/normal/recipes/enigmatica/remove.js
+++ kubejs/server_scripts/normal/recipes/enigmatica/remove.js
@@ -7,11 +7,6 @@ ServerEvents.recipes((event) => {
         // Emendatus Enigmatica Related Stuff for Normal Mode
         // Ore Processing Rework Related stuff
 
-        // { id: /create:crushing\/.*_ore/ },
-        // { id: /create:crushing\/.*raw_.*_block/},
-        // { id: /create:crushing\/.*raw_/},
-        // { id: /create:compat\/.*_ore/ },
-        // { id: /create:splashing\/.*crushed_.*_ore/ },
         { id: /emendatusenigmatica:slurry\/dirt/ },
         { id: /emendatusenigmatica:dust\/from_ore_crusher_spirit/ },
         { id: /emendatusenigmatica:dust\/from_ore_crush_spell/ },
@@ -20,29 +15,16 @@ ServerEvents.recipes((event) => {
         { id: /emendatusenigmatica:shard\/from_raw/ },
         { id: /emendatusenigmatica:clump\/from_ore/ },
         { id: /emendatusenigmatica:clump\/from_raw/ },
-        // { id: /immersiveengineering:crusher\/raw_.*/},
-        // { id: /immersiveengineering:crusher\/ore_.*/},
+
         { id: /immersiveengineering:arcfurnace\/ore/ },
         { id: /immersiveengineering:arcfurnace\/raw_ore/ },
         { id: /immersiveengineering:arcfurnace\/raw_block/ },
+
         { id: /thermal:machines\/smelter\/smelter.*_ore/ },
         { id: /thermal:machines\/smelter\/smelter.*_raw/ },
         { id: /thermal:compat\/.*_ore/, type: "thermal:smelter"},
         { id: /thermal:compat\/.*_raw_/, type: "thermal:smelter"},
-        // { id: 'thermal:machines/pulverizer/pulverizer_apatite'},
-        // { id: 'thermal:machines/pulverizer/pulverizer_cinnabar'},
-        // { id: 'thermal:machines/pulverizer/pulverizer_niter'},
-        // { id: 'thermal:machines/pulverizer/pulverizer_sulfur'},
-        // { id: /thermal:machines\/pulverizer\/pulverizer_.*_ingot_to_dust/},
-        // { id: /thermal:compat\/.*\/pulverizer_.*_ingot_to_dust/},
-        // { id: /thermal:machines\/pulverizer\/pulverizer.*_ore/ },
-        // { id: /thermal:machines\/pulverizer\/pulverizer.*_raw/ },
-        // { id: /thermal:compat\/.*_ore/, type: "thermal:pulverizer"},
-        // { id: /thermal:compat\/.*_raw_/, type: "thermal:pulverizer"},
-        // { id: /thermal:compat\/create.*\/centrifuge_create.*_crushed/ },
-        // { id: /mekanism:processing\/.*\/from_raw_block/ },
-        // { id: /mekanism:processing\/.*\/from_raw_ore/ },
-        // { id: /mekanism:processing\/.*\/from_ore/ },
+        
         { id: /occultism:crushing\/gold_dust/ }, // This is regex to also remove duplicates from raw ore
         { id: 'occultism:crushing/iesnium_dust' }, // There is a lot of different recipes with really similar ids (occultism:crushing/copper_dust_from_raw), so regex would delete all of those, what we don't want
         { id: 'occultism:crushing/copper_dust' }, 
