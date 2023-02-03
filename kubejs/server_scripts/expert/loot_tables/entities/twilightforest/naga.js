c94ec0a1373c8a5a0a9c1b25a276e6024dc9762b
MuteTiefling <mutetiefling@gmail.com>
2023-02-03 14:06:56 -0500

    hexerei and occultism plants

--- kubejs/server_scripts/expert/loot_tables/entities/twilightforest/naga.js
+++ kubejs/server_scripts/expert/loot_tables/entities/twilightforest/naga.js
@@ -7,5 +7,16 @@ ServerEvents.genericLootTables((event) => {
             pool.rolls = [5, 15];
             pool.addItem(Item.of('twilightforest:naga_scale'), 1);
         });
+
+        table.addPool((pool) => {
+            pool.rolls = [1];
+            pool.addItem(
+                Item.of(
+                    'minecraft:bundle',
+                    '{Items:[{Count:8b,id:"hexerei:yellow_dock_bush"},{Count:8b,id:"hexerei:mugwort_bush"},{Count:8b,id:"hexerei:sage_seed"},{Count:8b,id:"occultism:datura_seeds"},{Count:8b,id:"hexerei:belladonna_flower"},{Count:8b,id:"hexerei:mandrake_flower"},{Count:8b,id:"hexerei:willow_sapling"},{Count:8b,id:"hexerei:mahogany_sapling"}],display:{Lore:[\'{"text":"It seems whoever summoned this Naga didn\\\'t survive the process...","color":"dark_red"}\'],Name:\'{"text":"Druid\\\'s Herb Pouch","color":"dark_green"}\'}}'
+                ),
+                1
+            );
+        });
     });
 });
