20312c6482e50b5ba836002340d7cbcfa7745121
MuteTiefling <mutetiefling@gmail.com>
2023-02-10 13:25:54 -0500

    starting equipment nbt cleanup

--- kubejs/server_scripts/expert/player_events/starting_items.js
+++ kubejs/server_scripts/expert/player_events/starting_items.js
@@ -7,10 +7,12 @@ PlayerEvents.loggedIn((event) => {
         event.player.stages.add('starting_items');
 
         let starting_items = [
-            Item.of(
-                'minecraft:bow',
-                '{Damage:0,"quark:RuneAttached":1b,"quark:RuneColor":{Count:1b,id:"quark:blank_rune"}}'
-            ).enchant('minecraft:unbreaking', 1),
+            Item.of('minecraft:bow', {
+                Damage: 0,
+                'quark:RuneAttached': 1,
+                'quark:RuneColor': { Count: 1, id: 'quark:blank_rune' }
+            }).enchant('minecraft:unbreaking', 1),
+
             'supplementaries:quiver',
             '64x minecraft:arrow',
             '64x minecraft:arrow',
@@ -29,31 +31,39 @@ PlayerEvents.loggedIn((event) => {
         });
 
         event.player.setHeadArmorItem(
-            Item.of(
-                'minecraft:leather_helmet',
-                `{Damage:0,display:{Name:'{"text":"Sturdy Leather Cap","color":"dark_green"}',color:7441479},"quark:RuneAttached":1b,"quark:RuneColor":{Count:1b,id:"quark:blank_rune"}}`
-            ).enchant('minecraft:unbreaking', 1)
+            Item.of('minecraft:leather_helmet', {
+                Damage: 0,
+                display: { Name: '{"text":"Sturdy Leather Cap","color":"dark_green"}', color: 7441479 },
+                'quark:RuneAttached': 1,
+                'quark:RuneColor': { Count: 1, id: 'quark:blank_rune' }
+            }).enchant('minecraft:unbreaking', 1)
         );
 
         event.player.setChestArmorItem(
-            Item.of(
-                'minecraft:leather_chestplate',
-                `{Damage:0,display:{Name:'{"text":"Sturdy Leather Tunic","color":"dark_green"}',color:7441479},"quark:RuneAttached":1b,"quark:RuneColor":{Count:1b,id:"quark:blank_rune"}}`
-            ).enchant('minecraft:unbreaking', 1)
+            Item.of('minecraft:leather_chestplate', {
+                Damage: 0,
+                display: { Name: '{"text":"Sturdy Leather Tunic","color":"dark_green"}', color: 7441479 },
+                'quark:RuneAttached': 1,
+                'quark:RuneColor': { Count: 1, id: 'quark:blank_rune' }
+            }).enchant('minecraft:unbreaking', 1)
         );
 
         event.player.setLegsArmorItem(
-            Item.of(
-                'minecraft:leather_leggings',
-                `{Damage:0,display:{Name:'{"text":"Sturdy Leather Pants","color":"dark_green"}',color:7441479},"quark:RuneAttached":1b,"quark:RuneColor":{Count:1b,id:"quark:blank_rune"}}`
-            ).enchant('minecraft:unbreaking', 1)
+            Item.of('minecraft:leather_leggings', {
+                Damage: 0,
+                display: { Name: '{"text":"Sturdy Leather Pants","color":"dark_green"}', color: 7441479 },
+                'quark:RuneAttached': 1,
+                'quark:RuneColor': { Count: 1, id: 'quark:blank_rune' }
+            }).enchant('minecraft:unbreaking', 1)
         );
 
         event.player.setFeetArmorItem(
-            Item.of(
-                'minecraft:leather_boots',
-                `{Damage:0,display:{Name:'{"text":"Sturdy Leather Boots","color":"dark_green"}',color:7441479},"quark:RuneAttached":1b,"quark:RuneColor":{Count:1b,id:"quark:blank_rune"}}`
-            ).enchant('minecraft:unbreaking', 1)
+            Item.of('minecraft:leather_boots', {
+                Damage: 0,
+                display: { Name: '{"text":"Sturdy Leather Boots","color":"dark_green"}', color: 7441479 },
+                'quark:RuneAttached': 1,
+                'quark:RuneColor': { Count: 1, id: 'quark:blank_rune' }
+            }).enchant('minecraft:unbreaking', 1)
         );
     }
 });
