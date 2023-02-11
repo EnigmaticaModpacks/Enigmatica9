87bee4c1a6d993a55073701bb0fe787c722a08a0
MuteTiefling <mutetiefling@gmail.com>
2023-02-11 12:27:00 -0500

    Fix for replace input acting weird for carminite reactor
    was getting a cross of 5 source gems in the middle somehow...

--- kubejs/server_scripts/expert/recipes/enigmatica/replace_input.js
+++ kubejs/server_scripts/expert/recipes/enigmatica/replace_input.js
@@ -4,8 +4,16 @@ ServerEvents.recipes((event) => {
     }
 
     const recipes = [
-        { filter: { output: 'minecraft:hopper' }, to_replace: '#forge:ingots/iron', replace_with: '#forge:plates/tin' },
-        { filter: { output: 'minecraft:bucket' }, to_replace: '#forge:ingots/iron', replace_with: '#forge:plates/tin' },
+        {
+            filter: { output: 'minecraft:hopper' },
+            to_replace: '#forge:ingots/iron',
+            replace_with: '#forge:plates/tin'
+        },
+        {
+            filter: { output: 'minecraft:bucket' },
+            to_replace: '#forge:ingots/iron',
+            replace_with: '#forge:plates/tin'
+        },
         {
             filter: { mod: 'create' },
             to_replace: 'create:andesite_alloy',
@@ -21,13 +29,16 @@ ServerEvents.recipes((event) => {
             to_replace: 'create:andesite_alloy',
             replace_with: '#pneumaticcraft:compressed_stone'
         },
-        { filter: { mod: 'create' }, to_replace: 'minecraft:dried_kelp', replace_with: '#forge:leather' },
         {
-            filter: { id: 'twilightforest:carminite_reactor' },
-            to_replace: '#forge:ores/redstone',
-            replace_with: '#forge:storage_blocks/source'
+            filter: { mod: 'create' },
+            to_replace: 'minecraft:dried_kelp',
+            replace_with: '#forge:leather'
         },
-        { filter: { mod: 'pneumaticcraft' }, to_replace: '#c:glass_blocks', replace_with: 'ae2:quartz_glass' },
+        {
+            filter: { mod: 'pneumaticcraft' },
+            to_replace: '#c:glass_blocks',
+            replace_with: 'ae2:quartz_glass'
+        },
         {
             filter: { mod: 'pneumaticcraft', id: /wall_lamp/ },
             to_replace: '#forge:ingots/compressed_iron',
