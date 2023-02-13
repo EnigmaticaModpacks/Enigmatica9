09f69dd0c946b44c56774b04002477b6c67a355c
MuteTiefling <mutetiefling@gmail.com>
2023-02-13 16:29:42 -0500

    More create recipes reworked to deal with the lack of iron

--- kubejs/server_scripts/expert/recipes/create/shaped.js
+++ kubejs/server_scripts/expert/recipes/create/shaped.js
@@ -33,6 +33,115 @@ ServerEvents.recipes((event) => {
                 B: 'create:andesite_casing'
             },
             id: `create:crafting/kinetics/windmill_bearing`
+        },
+        {
+            output: 'create:millstone',
+            pattern: ['A', 'B', 'C'],
+            key: {
+                A: 'create:cogwheel',
+                B: 'create:andesite_casing',
+                C: 'pneumaticcraft:compressed_stone_slab'
+            },
+            id: `create:crafting/kinetics/millstone`
+        },
+        {
+            output: '3x create:andesite_tunnel',
+            pattern: [' A ', 'BBB', 'CCC'],
+            key: {
+                A: 'pneumaticcraft:logistics_core',
+                B: 'create:andesite_casing',
+                C: 'immersiveengineering:strip_curtain'
+            },
+            id: `create:crafting/logistics/andesite_tunnel`
+        },
+        {
+            output: '3x create:brass_tunnel',
+            pattern: [' A ', 'BBB', 'CCC'],
+            key: {
+                A: 'create:electron_tube',
+                B: 'create:brass_casing',
+                C: 'immersiveengineering:strip_curtain'
+            },
+            id: `create:crafting/logistics/brass_tunnel`
+        },
+        {
+            output: '2x create:andesite_funnel',
+            pattern: [' A ', 'CBC'],
+            key: {
+                A: 'pneumaticcraft:logistics_core',
+                B: 'create:andesite_casing',
+                C: 'immersiveengineering:strip_curtain'
+            },
+            id: `create:crafting/logistics/andesite_funnel`
+        },
+        {
+            output: '2x create:brass_funnel',
+            pattern: [' A ', 'CBC'],
+            key: {
+                A: 'create:electron_tube',
+                B: 'create:brass_casing',
+                C: 'immersiveengineering:strip_curtain'
+            },
+            id: `create:crafting/logistics/brass_funnel`
+        },
+        {
+            output: 'create:encased_chain_drive',
+            pattern: ['A', 'B', 'C'],
+            key: {
+                A: 'create:shaft',
+                B: 'create:andesite_casing',
+                C: 'minecraft:chain'
+            },
+            id: `create:crafting/kinetics/encased_chain_drive`
+        },
+        {
+            output: 'create:encased_chain_drive',
+            pattern: [' A ', ' B ', 'CCC'],
+            key: {
+                A: 'create:shaft',
+                B: 'create:andesite_casing',
+                C: '#forge:rope'
+            },
+            id: `${id_prefix}encased_chain_drive_from_rope`
+        },
+        {
+            output: '12x create:chute',
+            pattern: ['A A', 'A A', 'A A'],
+            key: {
+                A: 'immersiveengineering:sheetmetal_colored_black'
+            },
+            id: `create:crafting/kinetics/chute`
+        },
+        {
+            output: 'create:mechanical_press',
+            pattern: [' A ', 'DBD', ' C '],
+            key: {
+                A: 'create:shaft',
+                B: 'create:andesite_casing',
+                C: 'naturesaura:infused_stone',
+                D: 'ars_nouveau:earth_essence'
+            },
+            id: `create:crafting/kinetics/mechanical_press`
+        },
+        {
+            output: 'create:propeller',
+            pattern: [' B ', 'BAB', 'CB '],
+            key: {
+                A: 'create:shaft',
+                B: '#forge:plates/constantan',
+                C: 'ars_nouveau:air_essence'
+            },
+            id: `create:crafting/kinetics/propeller`
+        },
+        {
+            output: 'create:whisk',
+            pattern: [' C ', 'BAB', 'BAB'],
+            key: {
+                A: 'create:shaft',
+                B: '#forge:plates/bronze',
+                C: 'ars_nouveau:water_essence'
+            },
+            id: `create:crafting/kinetics/whisk`
         }
     ];
 
