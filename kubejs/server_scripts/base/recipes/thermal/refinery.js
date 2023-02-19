012e1dbeb51e9022f6c7aea653c102bc6c597b94
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-02-19 21:07:16 +0100

    fix thermal scripts for realsies

--- kubejs/server_scripts/base/recipes/thermal/refinery.js
+++ kubejs/server_scripts/base/recipes/thermal/refinery.js
@@ -1,49 +1,47 @@
-// ServerEvents.recipes((event) => {
-//     const id_prefix = 'enigmatica:base/thermal/refinery/';
+ServerEvents.recipes((event) => {
+    const id_prefix = 'enigmatica:base/thermal/refinery/';
 
-//     const recipes = [
-//         {
-//             input: { fluid: 'industrialforegoing:latex', amount: 900 },
-//             outputs: [
-//                 { fluid: 'minecraft:water', amount: 100 },
-//                 { item: 'thermal:rubber', chance: 1.0 }
-//             ],
-//             energy: 6000,
-//             id: `${id_prefix}rubber`
-//         },
-//         {
-//             input: { fluid: 'pneumaticcraft:oil', amount: 100 },
-//             outputs: [
-//                 { fluid: 'pneumaticcraft:diesel', amount: 50 },
-//                 { fluid: 'pneumaticcraft:gasoline', amount: 30 }
-//             ],
-//             energy: 7000,
-//             id: `${id_prefix}oil_to_diesel_and_lpg`
-//         },
-//         {
-//             input: { fluid: 'pneumaticcraft:diesel', amount: 100 },
-//             outputs: [{ fluid: 'pneumaticcraft:kerosene', amount: 80 }],
-//             energy: 7000,
-//             id: `${id_prefix}diesel_to_kerosene`
-//         },
-//         {
-//             input: { fluid: 'pneumaticcraft:kerosene', amount: 100 },
-//             outputs: [{ fluid: 'pneumaticcraft:gasoline', amount: 80 }],
-//             energy: 7000,
-//             id: `${id_prefix}kerosene_to_gasoline`
-//         },
-//         {
-//             input: { fluid: 'pneumaticcraft:gasoline', amount: 100 },
-//             outputs: [{ fluid: 'pneumaticcraft:lpg', amount: 80 }],
-//             energy: 7000,
-//             id: `${id_prefix}gasoline_to_lpg`
-//         }
-//     ];
+    const recipes = [
+        {
+            ingredient: { fluid: 'industrialforegoing:latex', amount: 900 },
+            result: [
+                { fluid: 'minecraft:water', amount: 100 },
+                { item: 'thermal:rubber', chance: 1.0 }
+            ],
+            energy: 6000,
+            id: `${id_prefix}rubber`
+        },
+        {
+            ingredient: { fluid: 'pneumaticcraft:oil', amount: 100 },
+            result: [
+                { fluid: 'pneumaticcraft:diesel', amount: 50 },
+                { fluid: 'pneumaticcraft:gasoline', amount: 30 }
+            ],
+            energy: 7000,
+            id: `${id_prefix}oil_to_diesel_and_lpg`
+        },
+        {
+            ingredient: { fluid: 'pneumaticcraft:diesel', amount: 100 },
+            result: [{ fluid: 'pneumaticcraft:kerosene', amount: 80 }],
+            energy: 7000,
+            id: `${id_prefix}diesel_to_kerosene`
+        },
+        {
+            ingredient: { fluid: 'pneumaticcraft:kerosene', amount: 100 },
+            result: [{ fluid: 'pneumaticcraft:gasoline', amount: 80 }],
+            energy: 7000,
+            id: `${id_prefix}kerosene_to_gasoline`
+        },
+        {
+            ingredient: { fluid: 'pneumaticcraft:gasoline', amount: 100 },
+            result: [{ fluid: 'pneumaticcraft:lpg', amount: 80 }],
+            energy: 7000,
+            id: `${id_prefix}gasoline_to_lpg`
+        }
+    ];
 
-//     recipes.forEach((recipe) => {
-//         recipe.type = 'thermal:refinery';
-//         recipe.ingredient = recipe.input;
-//         recipe.result = recipe.outputs;
-//         event.custom(recipe).id(recipe.id);
-//     });
-// });
+    recipes.forEach((recipe) => {
+        recipe.type = 'thermal:refinery';
+        event.custom(recipe).id(recipe.id);
+    });
+});
--- kubejs/server_scripts/base/recipes/thermal/smelter.js
+++ kubejs/server_scripts/base/recipes/thermal/smelter.js
@@ -1,23 +1,17 @@
-// ServerEvents.recipes((event) => {
-//     const id_prefix = 'enigmatica:base/thermal/smelter/';
+ServerEvents.recipes((event) => {
+    const id_prefix = 'enigmatica:base/thermal/smelter/';
 
-//     const recipes = [
-//         {
-//             inputs: [Ingredient.of('#forge:glass', 2), Ingredient.of('#forge:dusts/iron')],
-//             outputs: [Item.of('immersiveengineering:insulating_glass', 2)],
-//             energy: 3200,
-//             id: `${id_prefix}insulating_glass`
-//         }
-//     ];
+    const recipes = [
+        {
+            ingredients: [{ tag: 'forge:glass', count: 2 }, { tag: 'forge:dusts/iron' }],
+            result: [{ item: 'immersiveengineering:insulating_glass', count: 2 }],
+            energy: 3200,
+            id: `${id_prefix}insulating_glass`
+        }
+    ];
 
-//     recipes.forEach((recipe) => {
-//         recipe.type = 'thermal:smelter';
-
-//         // "ingredients": [{ "tag": "forge:ingots/copper" }, { "item": "thermal:press_coin_die" }]
-//         recipe.ingredients = recipe.inputs.map((input) => Ingredient.of(input).toJson());
-
-//         recipe.result = recipe.outputs.map((output) => output.toJson());
-
-//         event.custom(recipe).id(recipe.id);
-//     });
-// });
+    recipes.forEach((recipe) => {
+        recipe.type = 'thermal:smelter';
+        event.custom(recipe).id(recipe.id);
+    });
+});
