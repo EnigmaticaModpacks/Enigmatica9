de7ca2bea2cb526caf81bfd7ba609f3676c3b0cf
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-02-20 10:21:47 +0100

    Re-enable StoneCutting scripts, as they are working now with /reload at the startup

--- kubejs/server_scripts/base/recipes/minecraft/stonecutting.js
+++ kubejs/server_scripts/base/recipes/minecraft/stonecutting.js
@@ -10,23 +10,24 @@ ServerEvents.recipes((event) => {
     ];
 
     // Temporary Disabled due to bug with KubeJS -> Ingredient.of(tag).getItemIds() returns nothing at first launch!
-    // let conversionTypes = ['storage_block', 'ore'];
-    // let materials = Object.keys(metal_properties).concat(Object.keys(gemProperties))
-    // conversionTypes.forEach((type) => {
-    //    materials.forEach((material) => {
-    //         if (!entryIsBlacklisted(material, type)) {
-    //             let tag = Ingredient.of(`#forge:${type}s/${material}`);
-    //             let itemArray = tag.getItemIds()
-    //             if (itemArray.length > 1) {
-    //                 let index = 0
-    //                 itemArray.forEach((block) => {
-    //                     index++
-    //                     event.stonecutting(block, tag).id(`${id_prefix}${type}_conversion/${material}/${index}`);
-    //                 });
-    //             }
-    //         }
-    //     });
-    // });
+    // Override is in place! The game triggers /reload on first load so this script is going to work now :D
+    let conversionTypes = ['storage_block', 'ore'];
+    let materials = Object.keys(metal_properties).concat(Object.keys(gemProperties))
+    conversionTypes.forEach((type) => {
+       materials.forEach((material) => {
+            if (!entryIsBlacklisted(material, type)) {
+                let tag = Ingredient.of(`#forge:${type}s/${material}`);
+                let itemArray = tag.getItemIds()
+                if (itemArray.length > 1) {
+                    let index = 0
+                    itemArray.forEach((block) => {
+                        index++
+                        event.stonecutting(block, tag).id(`${id_prefix}${type}_conversion/${material}/${index}`);
+                    });
+                }
+            }
+        });
+    });
 
     recipes.forEach((recipe) => {
         event.stonecutting(recipe.output, recipe.input).id(recipe.id);
