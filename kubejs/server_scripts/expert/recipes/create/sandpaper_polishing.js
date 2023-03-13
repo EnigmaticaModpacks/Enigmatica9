20107dd028930680a5e811629a860daec89dc7ba
MuteTiefling <mutetiefling@gmail.com>
2023-03-12 22:36:53 -0400

    red brick polishing to base

--- kubejs/server_scripts/expert/recipes/create/sandpaper_polishing.js
+++ kubejs/server_scripts/expert/recipes/create/sandpaper_polishing.js
@@ -6,11 +6,11 @@ ServerEvents.recipes((event) => {
     const id_prefix = 'enigmatica:expert/create/sandpaper_polishing/';
 
     const recipes = [
-        {
-            ingredients: [{ item: 'minecraft:nether_brick' }],
-            results: [{ item: 'kubejs:red_nether_brick' }],
-            id: `${id_prefix}red_nether_brick`
-        }
+        // {
+        //     ingredients: [{ item: 'minecraft:nether_brick' }],
+        //     results: [{ item: 'kubejs:red_nether_brick' }],
+        //     id: `${id_prefix}red_nether_brick`
+        // }
     ];
 
     recipes.forEach((recipe) => {
