d97badf566e2daa0ca0a68df7b77243ece3cc3ee
MuteTiefling <mutetiefling@gmail.com>
2023-03-25 17:09:21 -0400

    gems

--- kubejs/server_scripts/base/recipes/apotheosis/gems/everbright.js
+++ kubejs/server_scripts/base/recipes/apotheosis/gems/everbright.js
@@ -6,6 +6,7 @@ ServerEvents.highPriorityData((event) => {
             name: 'moon_touched',
             weight: 30,
             quality: 0,
+            min_rarity: 'rare',
             dimensions: ['blue_skies:everbright'],
             bonuses: [
                 {
@@ -14,17 +15,15 @@ ServerEvents.highPriorityData((event) => {
                     attribute: 'apotheosis:cold_damage',
                     operation: 'ADDITION',
                     values: {
-                        common: { min: 0.25, steps: 2, step: 0.25 },
-                        uncommon: { min: 0.75, steps: 2, step: 0.25 },
                         rare: { min: 1.25, steps: 3, step: 0.5 },
-                        epic: { min: 2.75, steps: 3, step: 0.5 },
-                        mythic: { min: 4.25, steps: 4, step: 0.5 },
-                        ancient: { min: 6.25, steps: 6, step: 0.25 }
+                        epic: { min: 3.75, steps: 3, step: 0.5 },
+                        mythic: { min: 5.25, steps: 4, step: 0.5 },
+                        ancient: { min: 7.25, steps: 6, step: 0.25 }
                     }
                 },
                 {
                     type: 'apotheosis:attribute',
-                    gem_class: { key: 'core_armor', types: ['chestplate', 'leggings'] },
+                    gem_class: { key: 'boots', types: ['boots'] },
                     attribute: 'forge:entity_gravity',
                     operation: 'MULTIPLY_TOTAL',
                     values: {
@@ -37,18 +36,10 @@ ServerEvents.highPriorityData((event) => {
                     }
                 },
                 {
-                    type: 'apotheosis:attribute',
-                    gem_class: { key: 'boots', types: ['boots'] },
-                    attribute: 'forge:swim_speed',
-                    operation: 'MULTIPLY_BASE',
-                    values: {
-                        common: { min: 0.05, steps: 2, step: 0.025 },
-                        uncommon: { min: 0.1, steps: 2, step: 0.05 },
-                        rare: { min: 0.2, steps: 3, step: 0.05 },
-                        epic: { min: 0.35, steps: 3, step: 0.075 },
-                        mythic: { min: 0.6, steps: 4, step: 0.05 },
-                        ancient: { min: 0.8, steps: 6, step: 0.05 }
-                    }
+                    type: 'apotheosis:enchantment',
+                    gem_class: { key: 'core_armor', types: ['chestplate'] },
+                    enchantment: 'twilightforest:chill_aura',
+                    values: { rare: 1, epic: 1, mythic: 2, ancient: 3 }
                 }
             ]
         }
--- kubejs/server_scripts/base/recipes/apotheosis/gems/everdawn.js
+++ kubejs/server_scripts/base/recipes/apotheosis/gems/everdawn.js
@@ -6,6 +6,7 @@ ServerEvents.highPriorityData((event) => {
             name: 'sun_kissed',
             weight: 30,
             quality: 0,
+            min_rarity: 'rare',
             dimensions: ['blue_skies:everdawn'],
             bonuses: [
                 {
@@ -14,41 +15,31 @@ ServerEvents.highPriorityData((event) => {
                     attribute: 'apotheosis:fire_damage',
                     operation: 'ADDITION',
                     values: {
-                        common: { min: 0.25, steps: 2, step: 0.25 },
-                        uncommon: { min: 0.75, steps: 2, step: 0.25 },
                         rare: { min: 1.25, steps: 3, step: 0.5 },
-                        epic: { min: 2.75, steps: 3, step: 0.5 },
-                        mythic: { min: 4.25, steps: 4, step: 0.5 },
-                        ancient: { min: 6.25, steps: 6, step: 0.25 }
-                    }
-                },
-                {
-                    type: 'apotheosis:attribute',
-                    gem_class: { key: 'core_armor', types: ['chestplate', 'leggings'] },
-                    attribute: 'forge:entity_gravity',
-                    operation: 'MULTIPLY_TOTAL',
-                    values: {
-                        common: { min: 0.02, steps: 2, step: 0.02 },
-                        uncommon: { min: 0.06, steps: 2, step: 0.03 },
-                        rare: { min: 0.12, steps: 3, step: 0.04 },
-                        epic: { min: 0.24, steps: 3, step: 0.05 },
-                        mythic: { min: 0.4, steps: 4, step: 0.05 },
-                        ancient: { min: 0.8, steps: 6, step: 0.04 }
+                        epic: { min: 3.75, steps: 3, step: 0.5 },
+                        mythic: { min: 5.25, steps: 4, step: 0.5 },
+                        ancient: { min: 7.25, steps: 6, step: 0.25 }
                     }
                 },
                 {
                     type: 'apotheosis:attribute',
                     gem_class: { key: 'boots', types: ['boots'] },
-                    attribute: 'forge:step_height_addition',
-                    operation: 'ADDITION',
+                    attribute: 'forge:entity_gravity',
+                    operation: 'MULTIPLY_TOTAL',
                     values: {
-                        common: { min: 0.25, steps: 2, step: 0.125 },
-                        uncommon: { min: 0.5, steps: 2, step: 0.125 },
-                        rare: { min: 1.0, steps: 3, step: 0.125 },
-                        epic: { min: 1.5, steps: 3, step: 0.25 },
-                        mythic: { min: 3.0, steps: 4, step: 0.5 },
-                        ancient: { min: 5.0, steps: 6, step: 0.5 }
+                        common: { min: -0.02, steps: 2, step: -0.02 },
+                        uncommon: { min: -0.06, steps: 2, step: -0.03 },
+                        rare: { min: -0.12, steps: 3, step: -0.04 },
+                        epic: { min: -0.24, steps: 3, step: -0.05 },
+                        mythic: { min: -0.4, steps: 4, step: -0.05 },
+                        ancient: { min: -0.8, steps: 6, step: -0.04 }
                     }
+                },
+                {
+                    type: 'apotheosis:enchantment',
+                    gem_class: { key: 'core_armor', types: ['chestplate'] },
+                    enchantment: 'twilightforest:fire_react',
+                    values: { rare: 1, epic: 1, mythic: 2, ancient: 3 }
                 }
             ]
         }
