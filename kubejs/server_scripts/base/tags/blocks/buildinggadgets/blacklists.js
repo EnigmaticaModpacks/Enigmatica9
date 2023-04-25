f014db89117aae3c951170df9dd3a27e17d3d283
MuteTiefling <mutetiefling@gmail.com>
2023-04-25 11:29:03 -0400

    Set up Blacklist for Building Gadgets
    Resolves #295

---
+++ kubejs/server_scripts/base/tags/blocks/buildinggadgets/blacklists.js
@@ -0,0 +1,33 @@
+ServerEvents.tags('block', (event) => {
+    // Documentation: https://github.com/Direwolf20-MC/BuildingGadgets/tree/master/src/main/resources/data/buildinggadgets/tags/blocks/blacklist
+
+    const blocks = [
+        'immersiveengineering:alloy_smelter',
+        'immersiveengineering:arc_furnace',
+        'immersiveengineering:assembler',
+        'immersiveengineering:auto_workbench',
+        'immersiveengineering:bottling_machine',
+        'immersiveengineering:bucket_wheel',
+        'immersiveengineering:coke_oven',
+        'immersiveengineering:blast_furnace',
+        'immersiveengineering:crusher',
+        'immersiveengineering:diesel_generator',
+        'immersiveengineering:excavator',
+        'immersiveengineering:fermenter',
+        'immersiveengineering:advanced_blast_furnace',
+        'immersiveengineering:lightning_rod',
+        'immersiveengineering:metal_press',
+        'immersiveengineering:mixer',
+        'immersiveengineering:refinery',
+        'immersiveengineering:sawmill',
+        'immersiveengineering:silo',
+        'immersiveengineering:squeezer',
+        'immersiveengineering:tank',
+        'xnet:facade',
+        /sophisticatedbackpacks:.*backpack/,
+        /pneumaticcraft:.*pressure_tube/
+    ];
+    event.get('buildinggadgets:blacklist/building').add(blocks);
+    event.get('buildinggadgets:blacklist/copy_paste').add(blocks);
+    event.get('buildinggadgets:blacklist/exchanging').add(blocks);
+});
