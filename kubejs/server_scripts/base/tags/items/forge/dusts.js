b74e1987d7aa0732f941411a6d181bd02af05d65
MuteTiefling <mutetiefling@gmail.com>
2023-04-26 23:48:51 -0400

    dust to crystal

--- kubejs/server_scripts/base/tags/items/forge/dusts.js
+++ kubejs/server_scripts/base/tags/items/forge/dusts.js
@@ -1,4 +1,7 @@
 ServerEvents.tags('item', (event) => {
-    event.get('forge:dusts').add(['kubejs:amethyst_dust']);
+    event.get('forge:dusts').add(['kubejs:amethyst_dust', 'kubejs:subzero_dust', 'kubejs:therium_dust']);
+
     event.get('forge:dusts/amethyst').add(['kubejs:amethyst_dust']);
+    event.get('forge:dusts/subzero').add(['kubejs:subzero_dust']);
+    event.get('forge:dusts/therium').add(['kubejs:therium_dust']);
 });
