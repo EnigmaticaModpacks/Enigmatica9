25d41455c979f7d0f470d05752ff30a9396ad142
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-05-04 11:47:57 +0200

    Update gold.js

--- kubejs/server_scripts/base/tags/items/emendatusenigmatica/gold.js
+++ kubejs/server_scripts/base/tags/items/emendatusenigmatica/gold.js
@@ -1,1 +1,10 @@
-//To Do: Delete next breaking update
+ServerEvents.tags('item', (event) => {
+    event.add('minecraft:piglin_loved', [
+        'emendatusenigmatica:gold_nugget',
+        'emendatusenigmatica:gold_plate',
+        'emendatusenigmatica:gold_rod',
+        'emendatusenigmatica:gold_gear',
+        /*'emendatusenigmatica:crushed_gold_ore',*/
+        'emendatusenigmatica:gold_dust'
+    ]);
+});
