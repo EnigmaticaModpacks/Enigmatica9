ee04925a812bd612ad5a0918f359b6cb1b335b8f
MuteTiefling <mutetiefling@gmail.com>
2023-05-24 15:14:27 -0400

    armor buff tests

--- kubejs/server_scripts/expert/player_events/armor_sets.js
+++ kubejs/server_scripts/expert/player_events/armor_sets.js
@@ -1,45 +1,122 @@
-// PlayerEvents.inventoryChanged((event) => {
-//     const player = event.player;
-//     if (!player.isPlayer() || player.isFake()) {
-//         return;
-//     }
+const armor_sets = [
+    {
+        armor: [
+            'pneumaticcraft:pneumatic_helmet',
+            'pneumaticcraft:pneumatic_chestplate',
+            'pneumaticcraft:pneumatic_leggings',
+            'pneumaticcraft:pneumatic_boots'
+        ],
+        effects: [{ potion: 'ars_nouveau:mana_regen', amp: 2 }]
+    },
 
-//     if (event.item.id == 'tiab:time_in_a_bottle') {
-//         return;
-//     }
-
-//     let head = player.getHeadArmorItem().id;
-//     let chest = player.getChestArmorItem().id;
-//     let legs = player.getLegsArmorItem().id;
-//     let feet = player.getFeetArmorItem().id;
-
-//     // player.tell(['Equipped ', Text.yellow(head), Text.yellow(chest), Text.yellow(legs), Text.yellow(feet)]);
-
-//     if (head == 'pneumaticcraft:pneumatic_helmet') {
-//         player.potionEffects.add('ars_nouveau:recovery', 500, 3, false, false);
-//     }
-//     // event.player.inventory.removeItem(event.getSlot(), event.item.count);
-// });
-
-PlayerEvents.tick((event) => {
-    const player = event.player;
-    if (!player.isPlayer() || player.isFake()) {
-        return;
+    // Wearing any piece of the meka suit disables spell casting since it makes you effectively immune to magic.
+    {
+        armor: ['mekanism:mekasuit_helmet', null, null, null],
+        effects: [
+            { potion: 'ars_elemental:hymn_of_order', amp: 0 },
+            { potion: 'ars_nouveau:summoning_sickness', amp: 0 }
+        ]
+    },
+    {
+        armor: [null, 'mekanism:mekasuit_bodyarmor', null, null],
+        effects: [
+            { potion: 'ars_elemental:hymn_of_order', amp: 0 },
+            { potion: 'ars_nouveau:summoning_sickness', amp: 0 }
+        ]
+    },
+    {
+        armor: [null, null, 'mekanism:mekasuit_pants', null],
+        effects: [
+            { potion: 'ars_elemental:hymn_of_order', amp: 0 },
+            { potion: 'ars_nouveau:summoning_sickness', amp: 0 }
+        ]
+    },
+    {
+        armor: [null, null, null, 'mekanism:mekasuit_boots'],
+        effects: [
+            { potion: 'ars_elemental:hymn_of_order', amp: 0 },
+            { potion: 'ars_nouveau:summoning_sickness', amp: 0 }
+        ]
     }
+];
+
+const ars_elemental_types = ['aqua', 'air', ' fire', 'earth'];
+
+ars_elemental_types.forEach((type) => {
+    armor_sets.push({
+        armor: [
+            `ars_elemental:${type}_hat`,
+            `ars_elemental:${type}_robes`,
+            `ars_elemental:${type}_leggings`,
+            `ars_elemental:${type}_boots`
+        ],
+        effects: [{ potion: 'ars_nouveau:spell_damage', amp: 2 }]
+    });
+});
+
+armor_sets.forEach((armor_set) => {
+    PlayerEvents.tick((event) => {
+        const player = event.player;
+        const playerData = player.persistentData;
+        if (!player.isPlayer() || player.isFake()) {
+            return;
+        }
+
+        if (isSetValid(player, armor_set.armor)) {
+            playerData.armorSetBonus = true;
+            playerData.armorSetEffects = armor_set.effects;
+        } else {
+            playerData.armorSetBonus = false;
+        }
+
+        if (playerData.armorSetBonus) {
+            playerData.armorSetEffects.forEach((effect) => {
+                if (player.potionEffects.getDuration(effect.potion) <= 21) {
+                    player.potionEffects.add(effect.potion, 39, effect.amp, false, false);
+                }
+            });
+        }
+    });
+});
+
+function isSetValid(player, armor) {
+    let head = armor[0];
+    let chest = armor[1];
+    let legs = armor[2];
+    let feet = armor[3];
+
+    // if (head == null || player.getHeadArmorItem().id == head) {
+    //     head = true;
+    // } else {
+    //     head = false;
+    // }
+
+    // if (chest == null || player.getChestArmorItem().id == chest) {
+    //     chest = true;
+    // } else {
+    //     chest = false;
+    // }
+
+    // if (legs == null || player.getLegsArmorItem().id == legs) {
+    //     legs = true;
+    // } else {
+    //     legs = false;
+    // }
+
+    // if (feet == null || player.getFeetArmorItem().id == feet) {
+    //     feet = true;
+    // } else {
+    //     feet = false;
+    // }
 
     if (
-        player.getHeadArmorItem().id == 'pneumaticcraft:pneumatic_helmet' &&
-        player.getChestArmorItem().id == 'pneumaticcraft:pneumatic_chestplate' &&
-        player.getLegsArmorItem().id == 'pneumaticcraft:pneumatic_leggings' &&
-        player.getFeetArmorItem().id == 'pneumaticcraft:pneumatic_boots'
+        (head == null || player.getHeadArmorItem().id == head) &&
+        (chest == null || player.getChestArmorItem().id == chest) &&
+        (legs == null || player.getLegsArmorItem().id == legs) &&
+        (feet == null || player.getFeetArmorItem().id == feet)
     ) {
-        player.potionEffects.add('ars_nouveau:recovery', 999999, 1, false, false);
-        player.persistentData.armorSetBonus = true;
+        return true;
     } else {
-        player.persistentData.armorSetBonus = false;
+        return false;
     }
-
-    if (player.persistentData.armorSetBonus == false) {
-        player.potionEffects.add('ars_nouveau:recovery', 1, 2, false, false);
-    }
-});
+}
