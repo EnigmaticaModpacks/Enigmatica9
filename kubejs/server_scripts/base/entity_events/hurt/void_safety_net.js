a9638427792a6f2cd29c96c300f44a0f8ef80ce6
MuteTiefling <mutetiefling@gmail.com>
2023-05-26 12:53:08 -0400

    convert to entity.hurt event

---
+++ kubejs/server_scripts/base/entity_events/hurt/void_safety_net.js
@@ -0,0 +1,28 @@
+EntityEvents.hurt((event) => {
+    const { entity, source, level, server } = event;
+    if (!entity.isPlayer() || entity.isFake()) {
+        return;
+    }
+
+    let player = entity;
+
+    let currentDimension = String(level.getDimension());
+    let watchDimensions = ['compact_world', 'nomadictents', 'the_end'];
+    // console.log(source.type);
+    if (
+        source.type == 'outOfWorld' &&
+        player.y < 0 &&
+        watchDimensions.some((substring) => currentDimension.includes(substring))
+    ) {
+        let playerString = player.getUsername();
+        let spawn = player.getRespawnPosition();
+        let spawnDimension = player.getRespawnDimension().location();
+
+        console.log(`${playerString} has fallen into the void in ${currentDimension}!`);
+        console.log(`Teleporting to their spawn point in ${spawnDimension} at X:${spawn.x} Y:${spawn.y} Z:${spawn.z}`);
+
+        server.runCommandSilent(`execute in ${spawnDimension} run tp ${playerString} ${spawn.x} ${spawn.y} ${spawn.z}`);
+        player.tell(Text.of('Oops! Looks like you slipped! Warping back to your spawn point.').green());
+        event.cancel();
+    }
+});
