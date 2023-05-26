19c32e75dd44d12159e4e198a464237601c14aa3
MuteTiefling <mutetiefling@gmail.com>
2023-05-26 07:00:27 -0400

    Update nomadictents_safety.js

--- kubejs/server_scripts/base/player_events/nomadictents_safety.js
+++ kubejs/server_scripts/base/player_events/nomadictents_safety.js
@@ -1,18 +1,18 @@
 PlayerEvents.tick((event) => {
     const player = event.player;
+    const server = event.server;
     if (!player.isPlayer() || player.isFake()) {
         return;
     }
 
-    if (String(event.level.getDimension()).includes('nomadictents') && player.y < 30) {
+    if (String(event.level.getDimension()).includes('nomadictents') && player.y < 50) {
         let playerString = player.getUsername();
         let spawn = player.getRespawnPosition();
         let spawnDimension = player.getRespawnDimension().location();
         console.log(
             `${playerString} has fallen out of their tent! Teleporting to their spawn point in ${spawnDimension} at X:${spawn.x} Y:${spawn.y} Z:${spawn.z} `
         );
-        event.server.runCommandSilent(
-            `execute in ${spawnDimension} run tp ${playerString} ${spawn.x} ${spawn.y} ${spawn.z}`
-        );
+        server.runCommandSilent(`execute in ${spawnDimension} run tp ${playerString} ${spawn.x} ${spawn.y} ${spawn.z}`);
+        player.tell(Text.of('Oops! Looks like you slipped there. Warping back to your spawn point!').green());
     }
 });
