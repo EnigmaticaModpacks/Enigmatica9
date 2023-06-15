6c4a253b85fb541ba53507613dcd0905064da81a
MuteTiefling <mutetiefling@gmail.com>
2023-06-15 09:12:27 -0400

    mastery trofers

--- kubejs/server_scripts/expert/recipes/trofers/trofers.js
+++ kubejs/server_scripts/expert/recipes/trofers/trofers.js
@@ -5,76 +5,120 @@ ServerEvents.highPriorityData((event) => {
     const id_prefix = 'trofers:trofers/';
     const recipes = [
         {
-            colors: { accent: '#7b28bf', base: '#606060' },
-            display: { offset: { y: 2.0 }, scale: 0.625 },
+            colors: { accent: '#3dcc61', base: '#606060' },
+            display: { scale: 0.625 },
             item: { item: 'kubejs:tree_of_life_1' },
             name: {
-                color: '#7b28bf',
+                color: '#3dcc61',
                 translate: 'trophy.trofers.composed',
                 with: [{ translate: 'item.kubejs.tree_of_life_1' }]
             },
             effects: {
                 rewards: {
                     lootTable: 'enigmatica:loot_boxes/aura_leaves/tiny',
-                    cooldown: 60
+                    cooldown: 6000
                 }
             },
-            animation: { type: 'spinning', speed: 0.3 },
             id: 'tree_of_life_1'
         },
         {
-            colors: { accent: '#0f9440', base: '#606060' },
-            display: { offset: { y: 2.0 }, scale: 0.75 },
+            colors: { accent: '#3dcc61', base: '#606060' },
+            display: { scale: 0.75 },
             item: { item: 'kubejs:tree_of_life_2' },
             name: {
-                color: '#0f9440',
+                color: '#3dcc61',
                 translate: 'trophy.trofers.composed',
                 with: [{ translate: 'item.kubejs.tree_of_life_2' }]
             },
             effects: {
                 rewards: {
                     lootTable: 'enigmatica:loot_boxes/aura_leaves/small',
-                    cooldown: 60
+                    cooldown: 6000
                 }
             },
-            animation: { type: 'spinning', speed: 0.3 },
             id: 'tree_of_life_2'
         },
         {
-            colors: { accent: '#a61629', base: '#606060' },
-            display: { offset: { y: 2.0 }, scale: 0.875 },
+            colors: { accent: '#3dcc61', base: '#606060' },
+            display: { scale: 0.875 },
             item: { item: 'kubejs:tree_of_life_3' },
             name: {
-                color: '#a61629',
+                color: '#3dcc61',
                 translate: 'trophy.trofers.composed',
                 with: [{ translate: 'item.kubejs.tree_of_life_3' }]
             },
             effects: {
                 rewards: {
                     lootTable: 'enigmatica:loot_boxes/aura_leaves/medium',
-                    cooldown: 60
+                    cooldown: 6000
                 }
             },
-            animation: { type: 'spinning', speed: 0.3 },
             id: 'tree_of_life_3'
         },
         {
-            colors: { accent: '#f5f267', base: '#606060' },
-            display: { offset: { y: 2.0 }, scale: 1.0 },
+            colors: { accent: '#3dcc61', base: '#606060' },
+            display: { scale: 1.0 },
             item: { item: 'kubejs:tree_of_life_4' },
             name: {
-                color: '#f5f267',
+                color: '#3dcc61',
                 translate: 'trophy.trofers.composed',
                 with: [{ translate: 'item.kubejs.tree_of_life_4' }]
             },
             effects: {
                 rewards: {
                     lootTable: 'enigmatica:loot_boxes/aura_leaves/large',
-                    cooldown: 60
+                    cooldown: 6000
                 }
             },
-            animation: { type: 'spinning', speed: 0.3 },
             id: 'tree_of_life_4'
+        },
+        {
+            colors: { accent: '#e0d022', base: '#606060' },
+            display: { offset: { y: 2.0 }, scale: 1.0 },
+            item: { item: 'kubejs:mastery_token_air' },
+            name: {
+                color: '#e0d022',
+                translate: 'trophy.trofers.composed',
+                with: [{ translate: 'item.kubejs.mastery_token_air' }]
+            },
+            animation: { type: 'spinning', speed: 0.3 },
+            id: 'mastery_token_air'
+        },
+        {
+            colors: { accent: '#2d69e1', base: '#606060' },
+            display: { offset: { y: 2.0 }, scale: 1.0 },
+            item: { item: 'kubejs:mastery_token_water' },
+            name: {
+                color: '#2d69e1',
+                translate: 'trophy.trofers.composed',
+                with: [{ translate: 'item.kubejs.mastery_token_water' }]
+            },
+            animation: { type: 'spinning', speed: 0.3 },
+            id: 'mastery_token_water'
+        },
+        {
+            colors: { accent: '#bd4f00', base: '#606060' },
+            display: { offset: { y: 2.0 }, scale: 1.0 },
+            item: { item: 'kubejs:mastery_token_fire' },
+            name: {
+                color: '#bd4f00',
+                translate: 'trophy.trofers.composed',
+                with: [{ translate: 'item.kubejs.mastery_token_fire' }]
+            },
+            animation: { type: 'spinning', speed: 0.3 },
+            id: 'mastery_token_fire'
+        },
+        {
+            colors: { accent: '#016b5d', base: '#606060' },
+            display: { offset: { y: 2.0 }, scale: 1.0 },
+            item: { item: 'kubejs:mastery_token_earth' },
+            name: {
+                color: '#016b5d',
+                translate: 'trophy.trofers.composed',
+                with: [{ translate: 'item.kubejs.mastery_token_earth' }]
+            },
+            animation: { type: 'spinning', speed: 0.3 },
+            id: 'mastery_token_earth'
         }
     ];
 
