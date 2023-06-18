ae90976cd6611a1b411fbf8538456a46ef5317f6
MuteTiefling <mutetiefling@gmail.com>
2023-06-18 14:13:47 -0400

    book

--- kubejs/server_scripts/expert/recipes/ars_nouveau/book_upgrade.js
+++ kubejs/server_scripts/expert/recipes/ars_nouveau/book_upgrade.js
@@ -7,7 +7,11 @@ ServerEvents.recipes((event) => {
     const recipes = [
         {
             result: { item: 'ars_nouveau:apprentice_spell_book' },
-            ingredients: [{ item: 'ars_nouveau:novice_spell_book' }, { tag: 'forge:gems/spirit_attuned' }],
+            ingredients: [
+                { item: 'ars_nouveau:novice_spell_book' },
+                { item: 'naturesaura:calling_spirit' },
+                { tag: 'forge:gems/spirit_attuned' }
+            ],
             id: 'ars_nouveau:apprentice_spell_book_upgrade'
         },
         {
