6330eb067bb2888b6c1eb2ce8e78dcbf01991e8d
MuteTiefling <mutetiefling@gmail.com>
2023-06-22 14:45:14 -0400

    clear weather for players sleeping in twilight

---
+++ kubejs/server_scripts/base/block_events/clear_weather.js
@@ -0,0 +1,18 @@
+BlockEvents.rightClicked((event) => {
+    const { entity, block, level, server } = event;
+    if (!entity.isPlayer() || entity.isFake()) {
+        return;
+    }
+
+    const dimension = String(level.getDimension());
+    if (
+        dimension == 'twilightforest:twilight_forest' &&
+        block.getId().match(/(minecraft:|comforts:).*(_bed|sleeping_bag|hammock_)/)
+    ) {
+        // clear weather after 100 ticks
+        event.server.scheduleInTicks(100, (schedule) => {
+            let command = `/execute in minecraft:overworld run weather clear`;
+            schedule.server.runCommandSilent(command);
+        });
+    }
+});
