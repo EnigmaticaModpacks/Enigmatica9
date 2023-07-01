24b9788529c2e113c4f3e1344732409bd276f7ee
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-07-01 21:52:16 +0200

    converted `crystallizer.js` to use raw json

--- kubejs/server_scripts/normal/recipes/thermal/crystallizer.js
+++ kubejs/server_scripts/normal/recipes/thermal/crystallizer.js
@@ -6,44 +6,62 @@ ServerEvents.recipes((event) => {
 
     const recipes = [
         {
-            inputs: ['ae2:charged_certus_quartz_crystal', '#forge:dusts/amethyst'],
-            fluid: Fluid.of('minecraft:water', 2000),
-            outputs: [Item.of('ae2:fluix_crystal', 2)],
+            ingredients: [
+                { item: 'ae2:charged_certus_quartz_crystal' },
+                { tag: 'forge:dusts/amethyst' },
+                { fluid: 'minecraft:water', amount: 2000 }
+            ],
+            result: [{ item: 'ae2:fluix_crystal', count: 2 }],
             energy: 20000,
             id: `${id_prefix}fluix_crystal_from_amethyst_dust`
         },
         {
-            inputs: ['ae2:charged_certus_quartz_crystal', 'ae2:fluix_dust'],
-            fluid: Fluid.of('minecraft:water', 1000),
-            outputs: [Item.of('ae2:fluix_crystal')],
+            ingredients: [
+                { item: 'ae2:charged_certus_quartz_crystal' },
+                { item: 'ae2:fluix_dust' },
+                { fluid: 'minecraft:water', amount: 1000 }
+            ],
+            result: [{ item: 'ae2:fluix_crystal' }],
             energy: 10000,
             id: `${id_prefix}fluix_crystal_from_fluix_dust`
         },
         {
-            inputs: ['ae2:charged_certus_quartz_crystal', 'ae2:quartz_block'],
-            fluid: Fluid.of('minecraft:water', 500),
-            outputs: [Item.of('ae2:damaged_budding_quartz')],
+            ingredients: [
+                { item: 'ae2:charged_certus_quartz_crystal' },
+                { item: 'ae2:quartz_block' },
+                { fluid: 'minecraft:water', amount: 500 }
+            ],
+            result: [{ item: 'ae2:damaged_budding_quartz' }],
             energy: 5000,
             id: `${id_prefix}damaged_budding_quartz`
         },
         {
-            inputs: ['ae2:charged_certus_quartz_crystal', 'ae2:damaged_budding_quartz'],
-            fluid: Fluid.of('minecraft:water', 500),
-            outputs: [Item.of('ae2:chipped_budding_quartz')],
+            ingredients: [
+                { item: 'ae2:charged_certus_quartz_crystal' },
+                { item: 'ae2:damaged_budding_quartz' },
+                { fluid: 'minecraft:water', amount: 500 }
+            ],
+            result: [{ item: 'ae2:chipped_budding_quartz' }],
             energy: 5000,
             id: `${id_prefix}chipped_budding_quartz`
         },
         {
-            inputs: ['ae2:charged_certus_quartz_crystal', 'ae2:chipped_budding_quartz'],
-            fluid: Fluid.of('minecraft:water', 500),
-            outputs: [Item.of('ae2:flawed_budding_quartz')],
+            ingredients: [
+                { item: 'ae2:charged_certus_quartz_crystal' },
+                { item: 'ae2:chipped_budding_quartz' },
+                { fluid: 'minecraft:water', amount: 500 }
+            ],
+            result: [{ item: 'ae2:flawed_budding_quartz' }],
             energy: 5000,
             id: `${id_prefix}flawed_budding_quartz`
         },
         {
-            inputs: ['ae2:charged_certus_quartz_crystal', '#forge:dusts/certus_quartz'],
-            fluid: Fluid.of('minecraft:water', 2000),
-            outputs: [Item.of('ae2:certus_quartz_crystal', 2)],
+            ingredients: [
+                { item: 'ae2:charged_certus_quartz_crystal' },
+                { tag: 'forge:dusts/certus_quartz' },
+                { fluid: 'minecraft:water', amount: 2000 }
+            ],
+            result: [{ item: 'ae2:certus_quartz_crystal', count: 2 }],
             energy: 20000,
             id: `${id_prefix}certus_quartz_crystal`
         }
@@ -53,9 +71,9 @@ ServerEvents.recipes((event) => {
         recipe.type = 'thermal:crystallizer';
 
         // "ingredients": [{ "tag": "forge:ingots/copper" }, { "item": "thermal:press_coin_die" }]
-        recipe.ingredients = recipe.inputs.map((input) => Ingredient.of(input).toJson());
+        recipe.ingredients = recipe.ingredients.map((input) => Ingredient.of(input).toJson());
         recipe.ingredients.push(recipe.fluid.toJson());
-        recipe.result = recipe.outputs.map((output) => output.toJson());
+        recipe.result = recipe.result.map((output) => output.toJson());
         event.custom(recipe).id(recipe.id);
     });
 });
