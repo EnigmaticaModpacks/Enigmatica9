2d0560d19002cde3f303172324945375767c4995
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-07-03 19:24:45 +0200

    bump to 1.8.1

--- automation/settings.ps1
+++ automation/settings.ps1
@@ -29,11 +29,11 @@ $MODPACK_NAME = "Enigmatica9"
 $CLIENT_NAME = "Enigmatica9"
 
 # Version Of The Modpack
-$MODPACK_VERSION = "1.8.0"
+$MODPACK_VERSION = "1.8.1"
 
 # Last Version Of The Modpack
 # Needed For Changelog Parsing
-$LAST_MODPACK_VERSION = "1.7.2"
+$LAST_MODPACK_VERSION = "1.8.0"
 
 # =====================================================================//
 #  CHANGELOG SETTINGS
