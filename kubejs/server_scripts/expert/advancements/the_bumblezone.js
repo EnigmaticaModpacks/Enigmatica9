ec4815163da523410d902b9a1b3c661d4bd310ba
MuteTiefling <mutetiefling@gmail.com>
2023-07-10 18:57:10 -0400

    Update the_bumblezone.js

--- kubejs/server_scripts/expert/advancements/the_bumblezone.js
+++ kubejs/server_scripts/expert/advancements/the_bumblezone.js
@@ -71,12 +71,6 @@ ServerEvents.highPriorityData((event) => {
                         target_advancement: 'the_bumblezone:the_bumblezone/the_queens_desire/otherworldly_mites'
                     }
                 },
-                target_advancement_done_9: {
-                    trigger: 'the_bumblezone:target_advancement_done',
-                    conditions: {
-                        target_advancement: 'the_bumblezone:the_bumblezone/the_queens_desire/peak_inefficiency'
-                    }
-                },
                 target_advancement_done_10: {
                     trigger: 'the_bumblezone:target_advancement_done',
                     conditions: {
