cd3a6b2dc08415132a0505af52931cf8d3971a2b
MuteTiefling <mutetiefling@gmail.com>
2023-07-16 09:35:41 -0400

    Update the_nether.js

--- kubejs/server_scripts/base/recipes/apotheosis/gems/the_nether.js
+++ kubejs/server_scripts/base/recipes/apotheosis/gems/the_nether.js
@@ -13,45 +13,82 @@ ServerEvents.highPriorityData((event) => {
             bonuses: [
                 {
                     type: 'apotheosis:attribute',
-                    gem_class: { key: 'heavy_weapon', types: ['heavy_weapon'] },
+                    gem_class: {
+                        key: 'heavy_weapon',
+                        types: ['heavy_weapon']
+                    },
                     attribute: 'apotheosis:fire_damage',
                     operation: 'ADDITION',
                     values: {
-                        rare: { min: 6.0, steps: 3, step: 0.5 },
-                        epic: { min: 8.0, steps: 3, step: 0.5 },
-                        mythic: { min: 10.0, steps: 4, step: 0.5 },
-                        ancient: { min: 13.5, steps: 6, step: 0.5 }
+                        rare: 6,
+                        epic: 8,
+                        mythic: 10,
+                        ancient: 12.5
+                    }
+                },
+                {
+                    type: 'apotheosis:mob_effect',
+                    gem_class: {
+                        key: 'light_weapon',
+                        types: ['sword', 'trident']
+                    },
+                    mob_effect: 'apotheosis:detonation',
+                    target: 'attack_target',
+                    values: {
+                        mythic: {
+                            duration: 100,
+                            amplifier: 0,
+                            cooldown: 1200
+                        },
+                        ancient: {
+                            duration: 100,
+                            amplifier: 1,
+                            cooldown: 1200
+                        }
+                    }
+                },
+                {
+                    type: 'apotheosis:enchantment',
+                    gem_class: {
+                        key: 'chestplate',
+                        types: ['chestplate']
+                    },
+                    enchantment: 'minecraft:fire_protection',
+                    values: {
+                        rare: 1,
+                        epic: 1,
+                        mythic: 2,
+                        ancient: 2
+                    }
+                },
+                {
+                    type: 'apotheosis:enchantment',
+                    gem_class: {
+                        key: 'breaker',
+                        types: ['shovel', 'pickaxe']
+                    },
+                    enchantment: 'minecraft:efficiency',
+                    values: {
+                        rare: 1,
+                        epic: 1,
+                        mythic: 2,
+                        ancient: 2
                     }
                 },
                 {
                     type: 'apotheosis:attribute',
-                    gem_class: { key: 'helmet', types: ['helmet'] },
+                    gem_class: {
+                        key: 'helmet',
+                        types: ['helmet']
+                    },
                     attribute: 'apotheosis:fire_damage',
                     operation: 'MULTIPLY_TOTAL',
                     values: {
-                        rare: { min: 0.5, steps: 3, step: 0.025 },
-                        epic: { min: 0.6, steps: 3, step: 0.025 },
-                        mythic: { min: 0.75, steps: 4, step: 0.025 },
-                        ancient: { min: 0.9, steps: 5, step: 0.02 }
+                        rare: 0.5,
+                        epic: 0.625,
+                        mythic: 0.75,
+                        ancient: 0.9
                     }
-                },
-                {
-                    type: 'apotheosis:enchantment',
-                    gem_class: { key: 'light_weapon', types: ['sword', 'trident'] },
-                    enchantment: 'minecraft:fire_aspect',
-                    values: { rare: 1, epic: 2, mythic: 3, ancient: 4 }
-                },
-                {
-                    type: 'apotheosis:enchantment',
-                    gem_class: { key: 'chestplate', types: ['chestplate'] },
-                    enchantment: 'minecraft:fire_protection',
-                    values: { rare: 1, epic: 1, mythic: 2, ancient: 2 }
-                },
-                {
-                    type: 'apotheosis:enchantment',
-                    gem_class: { key: 'breaker', types: ['shovel', 'pickaxe'] },
-                    enchantment: 'minecraft:efficiency',
-                    values: { rare: 1, epic: 1, mythic: 2, ancient: 2 }
                 }
             ]
         }
