2635d0254119e7060016f8c6511046c9979beb2c
MuteTiefling <mutetiefling@gmail.com>
2023-07-18 14:15:48 -0400

    Update crushing_tiers.js

--- kubejs/server_scripts/expert/recipes/enigmatica/crushing_tiers.js
+++ kubejs/server_scripts/expert/recipes/enigmatica/crushing_tiers.js
@@ -20,7 +20,7 @@ ServerEvents.recipes((event) => {
         {
             outputs: { primary: 'occultism:crushed_end_stone' },
             input: 'minecraft:end_stone',
-            crushing_tier: 3,
+            crushing_tier: 2,
             id_suffix: 'crushed_end_stone_from_end_stone'
         },
         {
