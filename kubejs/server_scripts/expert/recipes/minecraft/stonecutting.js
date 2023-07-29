a7a23af162664a7f75d1b62909b6b2dc3e476801
MuteTiefling <mutetiefling@gmail.com>
2023-07-29 01:08:09 -0400

    fix andesite scaffolding

--- kubejs/server_scripts/expert/recipes/minecraft/stonecutting.js
+++ kubejs/server_scripts/expert/recipes/minecraft/stonecutting.js
@@ -26,6 +26,11 @@ ServerEvents.recipes((event) => {
             id: `${id_prefix}andesite_bars`
         },
         {
+            output: '2x create:andesite_scaffolding',
+            input: 'pneumaticcraft:compressed_stone',
+            id: `${id_prefix}andesite_scaffolding`
+        },
+        {
             output: '4x create:copycat_panel',
             input: '#forge:ingots/ironwood',
             id: `${id_prefix}copycat_panel`
