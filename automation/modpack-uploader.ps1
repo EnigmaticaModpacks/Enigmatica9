1681fd9a4b74aa377b034636317e82c140d08d50
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2023-08-07 19:57:13 +0200

    fix bcc not getting version updated

--- automation/modpack-uploader.ps1
+++ automation/modpack-uploader.ps1
@@ -106,7 +106,7 @@ function Update-BetterCompatibilityCheckerVersion {
     }
 
     # Replace anything that matches semver of the type 1.0.0 with $MODPACK_VERSION
-    $contents = [System.IO.File]::ReadAllText($configPath) -replace "\d\.\d\.\d", $MODPACK_VERSION
+    $contents = [System.IO.File]::ReadAllText($configPath) -replace "\d+\.\d+\.\d+", $MODPACK_VERSION
     $contents = $contents -replace "modpackName.*", "modpackName = `"$modpackName`""
     $contents = $contents -replace "modpackProjectID.*", "modpackProjectID = $CURSEFORGE_PROJECT_ID"
 
