083400228bfc1f8179a9c7e0cf3df283a03b1a77
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-08-22 23:51:58 +0200

    More Setup stuff
    Add Arguments for Setup and launch from git hooks
    Added to GitIgnore CDL Logs folder and settings file
    Changed name of CDL to custom one without a version, so updates won't break the scripts

--- automation/Cat-Downloader-Legacy_Setup.bat
+++ automation/Cat-Downloader-Legacy_Setup.bat
@@ -1,17 +1,16 @@
 @echo off
 
 REM You set your arguments here
-set arguments_setup=
-set arguments_launch=
+set arguments_setup=-SettingsPath:automation -LogsPath:automation\\CDL-Logs -DefaultSettings:false -Mode:CF-Instance
+set arguments_launch=-SettingsPath:automation
 
-set version=2.1.0
 cd ..
 type NUL > .git/hooks/post-merge
 echo #!/bin/sh > .git/hooks/post-merge
-echo java -jar automation/Cat-Downloader-Legacy-%version%.jar %arguments_launch% >> .git/hooks/post-merge
+echo java -jar automation/Cat-Downloader-Legacy.jar %arguments_launch% >> .git/hooks/post-merge
 
-echo Git Hooks have been set up! Running Cat Downloader Legacy %version%...
+echo Git Hooks have been set up! Running Cat-Downloader-Legacy...
 
-java -jar automation/Cat-Downloader-Legacy-%version%.jar %arguments_setup%
+java -jar automation/Cat-Downloader-Legacy.jar %arguments_setup%
 
 pause
--- automation/Cat-Downloader-Legacy_Setup.sh
+++ automation/Cat-Downloader-Legacy_Setup.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-ARGUMENTS_SETUP=
-ARGUMENTS_LAUNCH=
+ARGUMENTS_SETUP= "-SettingsPath:automation -LogsPath:automation\\CDL-Logs -DefaultSettings:false -Mode:CF-Instance"
+ARGUMENTS_LAUNCH= "-SettingsPath:automation"
 
-VERSION="2.1.0"
 cd ..
 echo "#!/bin/sh" > .git/hooks/post-merge
-echo "java -jar automation/Cat-Downloader-Legacy-${VERSION}.jar ${ARGUMENTS_LAUNCH}" >> .git/hooks/post-merge
+echo "java -jar automation/Cat-Downloader-Legacy.jar ${ARGUMENTS_LAUNCH}" >> .git/hooks/post-merge
 
-echo Git Hooks have been set up! Running Cat Downloader Legacy ${VERSION}...
+echo Git Hooks have been set up! Running Cat-Downloader-Legacy...
 
-java -jar automation/Cat-Downloader-Legacy-${VERSION}.jar ${ARGUMENTS_SETUP}
+java -jar automation/Cat-Downloader-Legacy.jar ${ARGUMENTS_SETUP}
