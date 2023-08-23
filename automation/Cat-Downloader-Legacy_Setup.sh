8830890b820c38a937320c5bf518cb6819124a47
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-08-23 11:04:17 +0200

    Add missing command to the .sh script

--- automation/Cat-Downloader-Legacy_Setup.sh
+++ automation/Cat-Downloader-Legacy_Setup.sh
@@ -6,6 +6,7 @@ ARGUMENTS_LAUNCH="-SettingsPath:automation"
 cd ..
 echo "#!/bin/sh" > .git/hooks/post-merge
 echo "java -jar automation/Cat-Downloader-Legacy.jar ${ARGUMENTS_LAUNCH}" >> .git/hooks/post-merge
+chmod +x .git/hooks/post-merge
 
 echo Git Hooks have been set up! Running Cat-Downloader-Legacy...
 
