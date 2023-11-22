a9c700eb6bbc7803a58f951ded53b0c9dee03bb0
MuteTiefling <mutetiefling@gmail.com>
2023-11-22 15:21:22 -0500

    Update dissolution_chamber.js

--- kubejs/server_scripts/base/recipes/industrialforegoing/dissolution_chamber.js
+++ kubejs/server_scripts/base/recipes/industrialforegoing/dissolution_chamber.js
@@ -1,34 +1,24 @@
 ServerEvents.recipes((event) => {
     const id_prefix = 'enigmatica:base/industrialforegoing/dissolution_chamber/';
-    const recipes = [
-        {
+    const recipes = [];
+
+    let xp_fluids = ['cofh_core:experience', 'pneumaticcraft:memory_essence', 'sophisticatedcore:xp_still'];
+    xp_fluids.forEach((xp_fluid) => {
+        recipes.push({
             output: 'minecraft:experience_bottle',
-            inputFluid: '{Amount:250,FluidName:"sophisticatedcore:xp_still"}',
+            inputFluid: `{Amount:250,FluidName:"${xp_fluid}"}`,
             processingTime: 5,
-            id: `${id_prefix}experience_bottle_from_xp_still`
-        },
-        {
-            output: 'minecraft:experience_bottle',
-            inputFluid: '{Amount:250,FluidName:"pneumaticcraft:memory_essence"}',
-            processingTime: 5,
-            id: `${id_prefix}experience_bottle_from_memory_essence`
-        },
-        {
-            output: 'minecraft:experience_bottle',
-            inputFluid: '{Amount:250,FluidName:"cofh_core:experience"}',
-            processingTime: 5,
-            id: `${id_prefix}experience_bottle_from_experience`
-        }
-    ];
+            id: `${id_prefix}experience_bottle_from_${xp_fluid.replace(':', '_')}`
+        });
+    });
 
     recipes.forEach((recipe) => {
         recipe.type = 'industrialforegoing:dissolution_chamber';
+        recipe.input = [];
         if (recipe.inputs) {
             recipe.input = recipe.inputs.map((input) =>
                 input.startsWith('#') ? { tag: input.slice(1) } : { item: input }
             );
-        } else {
-            recipe.input = [];
         }
 
         recipe.output = Item.of(recipe.output).toJson();
