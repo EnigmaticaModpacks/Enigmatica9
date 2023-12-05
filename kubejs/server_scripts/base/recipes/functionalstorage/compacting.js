46d855ad82e9608f38e0fb2134c595339d0f7315
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-12-05 19:42:18 +0100

    Simplify the overcomplicated code :smile:

--- kubejs/server_scripts/base/recipes/functionalstorage/compacting.js
+++ kubejs/server_scripts/base/recipes/functionalstorage/compacting.js
@@ -3,11 +3,10 @@ ServerEvents.recipes((event) => {
     const recipes = [
         // Example:
         // {
-        //     higher_input: { item: 'minecraft:diamond_block'},
+        //     higher_input: 'minecraft:diamond_block',
         //     lower_input: '9x minecraft:diamond',
         //     id: `example_id`
-        // }
-        // Inputs support missing count object (defaults to 1) and String used to use in Item.of()
+        // },
         {
             higher_input: 'minecraft:amethyst_block',
             lower_input: '4x minecraft:amethyst_shard',
@@ -38,19 +37,8 @@ ServerEvents.recipes((event) => {
     recipes.forEach((recipe) => {
 
         recipe.type = 'functionalstorage:custom_compacting';
-    
-        if (typeof recipe.higher_input == "string") {
-            recipe.higher_input = Item.of(recipe.higher_input).toJson()
-        } else if (!Object.keys(recipe.higher_input).includes('count')) {
-            recipe.higher_input.count = 1;
-        }
-
-        if (typeof recipe.lower_input == "string") {
-            recipe.lower_input = Item.of(recipe.lower_input).toJson()
-        } else if (!Object.keys(recipe.lower_input).includes('count')) {
-            recipe.lower_input.count = 1;
-        }
-        
+        recipe.higher_input = Item.of(recipe.higher_input).toJson()
+        recipe.lower_input = Item.of(recipe.lower_input).toJson()
         event.custom(recipe).id(recipe.id);
     });
 });
