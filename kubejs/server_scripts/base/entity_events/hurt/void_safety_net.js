2c6d733bbe9fe7a05374e903d56d8430a5144644
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2024-01-12 16:33:33 +0100

    Fix void safety net latency issue

--- kubejs/server_scripts/base/entity_events/hurt/void_safety_net.js
+++ kubejs/server_scripts/base/entity_events/hurt/void_safety_net.js
@@ -1,8 +1,11 @@
 EntityEvents.hurt((event) => {
     const { entity, source, level, server } = event;
-    if (!entity.isPlayer() || entity.isFake()) {
-        return;
-    }
+    if (!entity.isPlayer() || entity.isFake()) return;
+
+    let currentTime = level.getDayTime();
+    if (server.persistentData["lastVoidTime_" + entity.getUsername()] && server.persistentData["lastVoidTime_" + entity.getUsername()] + 40 > currentTime) return;
+    server.persistentData["lastVoidTime_" + entity.getUsername()] = currentTime;
+
     let player = entity;
     let currentDimension = String(level.getDimension());
     let watchDimensions = ['compact_world', 'nomadictents', 'the_end'];
