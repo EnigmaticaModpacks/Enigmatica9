676c5a45e80166515b90e11c8c138173ae0f6021
MuteTiefling <mutetiefling@gmail.com>
2024-03-28 16:42:51 -0400

    typos

--- kubejs/server_scripts/base/tags/items/forge/stripped_logs.js
+++ kubejs/server_scripts/base/tags/items/forge/stripped_logs.js
@@ -1,3 +1,3 @@
 ServerEvents.tags('item', (event) => {
-    event.add('forge:stripped_logs', [/log_stripped/, /stripped_.*)(log|stem|pedu)/]);
+    event.add('forge:stripped_logs', [/log_stripped/, /stripped_.*_(log|stem|pedu)/]);
 });
