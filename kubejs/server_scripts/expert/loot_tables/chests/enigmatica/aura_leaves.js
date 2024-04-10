c8c4991a3939eae006e53a3d107b993053351e72
MuteTiefling <mutetiefling@gmail.com>
2024-04-10 17:32:41 -0400

    Buff Aura Infusion
    Apparently it's too small to really notice, so people assume they don't work :( Massive buff!

--- kubejs/server_scripts/expert/loot_tables/chests/enigmatica/aura_leaves.js
+++ kubejs/server_scripts/expert/loot_tables/chests/enigmatica/aura_leaves.js
@@ -6,28 +6,28 @@ ServerEvents.genericLootTables((event) => {
     event.addGeneric('enigmatica:loot_boxes/aura_leaves/tiny', (table) => {
         table.addPool((pool) => {
             pool.rolls = 1.0;
-            pool.addItem(Item.of('kubejs:aura_leaf', '{aura_amount:20000.0d,aura_max:2500000.0d}'), 1, 1);
+            pool.addItem(Item.of('kubejs:aura_leaf', '{aura_amount:300000.0d,aura_max:2500000.0d}'), 1, 1);
         });
     });
 
     event.addGeneric('enigmatica:loot_boxes/aura_leaves/small', (table) => {
         table.addPool((pool) => {
             pool.rolls = 1.0;
-            pool.addItem(Item.of('kubejs:aura_leaf', '{aura_amount:40000.0d,aura_max:2500000.0d}'), 1, 1);
+            pool.addItem(Item.of('kubejs:aura_leaf', '{aura_amount:600000.0d,aura_max:2500000.0d}'), 1, 1);
         });
     });
 
     event.addGeneric('enigmatica:loot_boxes/aura_leaves/medium', (table) => {
         table.addPool((pool) => {
             pool.rolls = 1.0;
-            pool.addItem(Item.of('kubejs:aura_leaf', '{aura_amount:60000.0d,aura_max:2500000.0d}'), 1, 1);
+            pool.addItem(Item.of('kubejs:aura_leaf', '{aura_amount:900000.0d,aura_max:2500000.0d}'), 1, 1);
         });
     });
 
     event.addGeneric('enigmatica:loot_boxes/aura_leaves/large', (table) => {
         table.addPool((pool) => {
             pool.rolls = 1.0;
-            pool.addItem(Item.of('kubejs:aura_leaf', '{aura_amount:250000.0d,aura_max:2500000.0d}'), 1, 1);
+            pool.addItem(Item.of('kubejs:aura_leaf', '{aura_amount:1200000.0d,aura_max:2500000.0d}'), 1, 1);
         });
     });
 });
